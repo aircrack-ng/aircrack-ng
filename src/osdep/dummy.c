e84b98e3ad7b0af2af329a86c79ca455de0ded3f
Andrea Bittau <a.bittau@cs.ucl.ac.uk>
2007-04-09 07:20:33 +0000

    Allow aircrack to build on unsupported platforms.  No RX/TX will be available.
    git-svn-id: http://svn.aircrack-ng.org/branch/0.9-dev@271 28c6078b-6c39-48e3-add9-af49d547ecab

---
+++ src/osdep/dummy.c
@@ -0,0 +1,29 @@
+/*- 
+ * Copyright (c) 2007, Andrea Bittau <a.bittau@cs.ucl.ac.uk>
+ *
+ * OS dependent API for unsupported APIs.
+ *
+ */
+
+#include <errno.h>
+#include <stdio.h>
+
+#include "osdep.h"
+
+struct wif *wi_open(char *iface)
+{
+	errno = EOPNOTSUPP;
+	return NULL;
+}
+
+int get_battery_state(void)
+{
+	errno = EOPNOTSUPP;
+	return -1;
+}
+
+int create_tap(void)
+{
+	errno = EOPNOTSUPP;
+	return -1;
+}
