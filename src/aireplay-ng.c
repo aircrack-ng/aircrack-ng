bda2ede4575141cd3dbf3f3294743998f754e8f9
Martin Beck <hirte@aircrack-ng.org>
2007-04-16 00:04:27 +0000

    Fixed zd1211rw injection.
    git-svn-id: http://svn.aircrack-ng.org/branch/0.9-dev@296 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/aireplay-ng.c
+++ src/aireplay-ng.c
@@ -887,6 +887,24 @@ int do_attack_fake_auth( void )
                     if( send_packet( h80211, 30 ) < 0 )
                         return( 1 );
 
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
                     if( send_packet( ackbuf, 14 ) < 0 )
                         return( 1 );
                 }
@@ -971,6 +989,24 @@ int do_attack_fake_auth( void )
                 {
                     if( send_packet( h80211, 46 + n ) < 0 )
                         return( 1 );
+
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
+
+                    if( send_packet( ackbuf, 14 ) < 0 )
+                        return( 1 );
+                    usleep(10);
 
                     if( send_packet( ackbuf, 14 ) < 0 )
                         return( 1 );
