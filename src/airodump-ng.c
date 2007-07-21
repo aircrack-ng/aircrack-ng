cfa0550390a2545305d557c4a9c5a7b6551e1441
Martin Beck <hirte@aircrack-ng.org>
2007-07-21 21:54:06 +0000

    Fixed airodump-ng opening the same interface more than once. (closes: #274)
    git-svn-id: http://svn.aircrack-ng.org/branch/1.0-dev@601 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airodump-ng.c
+++ src/airodump-ng.c
@@ -2815,6 +2815,7 @@ int init_cards(const char* cardstr, char *iface[], struct wif **wi)
 {
     char *buffer;
     int if_count=0;
+    int i=0, again=0;
 
     buffer = (char*) malloc( sizeof(char) * 1025 );
     strncpy( buffer, cardstr, 1025 );
@@ -2822,6 +2823,13 @@ int init_cards(const char* cardstr, char *iface[], struct wif **wi)
 
     while( ((iface[if_count]=strsep(&buffer, ",")) != NULL) && (if_count < MAX_CARDS) )
     {
+        again=0;
+        for(i=0; i<if_count; i++)
+        {
+            if(strcmp(iface[i], iface[if_count]) == 0)
+            again=1;
+        }
+        if(again) continue;
         if(setup_card(iface[if_count], &(wi[if_count])) != 0)
             return -1;
         if_count++;
