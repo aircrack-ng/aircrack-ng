bdcf3b6b86be50a493e5bc1bf207987551319f50
Martin Beck <hirte@aircrack-ng.org>
2007-09-24 21:07:02 +0000

    Added NetBSD osdep files. Should be really easy to get them working, can't test it for now, but the basic commands should be correct. It compiles cleanly on NetBSD. Also updated Changelog.
    git-svn-id: http://svn.aircrack-ng.org/branch/1.0-dev@733 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/osdep/openbsd_tap.c
+++ src/osdep/openbsd_tap.c
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 2007, Andrea Bittau <a.bittau@cs.ucl.ac.uk>
  *
- * OS dependent API for OpenBSD.  TAP routines.
+ * OS dependent API for Opeobsd.  TAP routines.
  *
  */
 
@@ -19,20 +19,20 @@
 
 #include "osdep.h"
 
-struct tip_fbsd {
-	int		tf_fd;
-	int		tf_ioctls;
-	struct ifreq	tf_ifr;
-	char		tf_name[MAX_IFACE_NAME];
-	int		tf_destroy;
+struct tip_obsd {
+	int		to_fd;
+	int		to_ioctls;
+	struct ifreq	to_ifr;
+	char		to_name[MAX_IFACE_NAME];
+	int		to_destroy;
 };
 
-static int ti_do_open_fbsd(struct tif *ti, char *name)
+static int ti_do_open_obsd(struct tif *ti, char *name)
 {
 	int fd;
 	char *iface = "/dev/tap";
 	struct stat st;
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 	int s;
 	unsigned int flags;
 	struct ifreq *ifr;
@@ -41,7 +41,7 @@ static int ti_do_open_fbsd(struct tif *ti, char *name)
 	if (name)
 		iface = name;
 	else
-		priv->tf_destroy = 1; /* we create, we destroy */
+		priv->to_destroy = 1; /* we create, we destroy */
 
 	fd = open(iface, O_RDWR);
 	if (fd == -1)
@@ -50,19 +50,19 @@ static int ti_do_open_fbsd(struct tif *ti, char *name)
 	/* get name */
 	if(fstat(fd, &st) == -1)
 		goto err;
-	snprintf(priv->tf_name, sizeof(priv->tf_name)-1, "%s",
+	snprintf(priv->to_name, sizeof(priv->to_name)-1, "%s",
 		 devname(st.st_rdev, S_IFCHR));
 
 	/* bring iface up */
 	s = socket(PF_INET, SOCK_DGRAM, 0);
 	if (s == -1)
 		goto err;
-	priv->tf_ioctls = s;
+	priv->to_ioctls = s;
 
 	/* get flags */
-	ifr = &priv->tf_ifr;
+	ifr = &priv->to_ifr;
 	memset(ifr, 0, sizeof(*ifr));
-	snprintf(ifr->ifr_name, sizeof(ifr->ifr_name)-1, "%s", priv->tf_name);
+	snprintf(ifr->ifr_name, sizeof(ifr->ifr_name)-1, "%s", priv->to_name);
 	if (ioctl(s, SIOCGIFFLAGS, ifr) == -1)
 		goto err2;
 	flags = ifr->ifr_flags;
@@ -85,113 +85,113 @@ err2:
 
 static void ti_do_free(struct tif *ti)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 
 	free(priv);
 	free(ti);
 }
 
-static void ti_destroy(struct tip_fbsd *priv)
+static void ti_destroy(struct tip_obsd *priv)
 {
-	ioctl(priv->tf_ioctls, SIOCIFDESTROY, &priv->tf_ifr);
+	ioctl(priv->to_ioctls, SIOCIFDESTROY, &priv->to_ifr);
 }
 
-static void ti_close_fbsd(struct tif *ti)
+static void ti_close_obsd(struct tif *ti)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 
-	if (priv->tf_destroy)
+	if (priv->to_destroy)
 		ti_destroy(priv);
 
-	close(priv->tf_fd);
-	close(priv->tf_ioctls);
+	close(priv->to_fd);
+	close(priv->to_ioctls);
 	ti_do_free(ti);
 }
 
-static char *ti_name_fbsd(struct tif *ti)
+static char *ti_name_obsd(struct tif *ti)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 
-	return priv->tf_name;
+	return priv->to_name;
 }
 
-static int ti_set_mtu_fbsd(struct tif *ti, int mtu)
+static int ti_set_mtu_obsd(struct tif *ti, int mtu)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 
-	priv->tf_ifr.ifr_mtu = mtu;
+	priv->to_ifr.ifr_mtu = mtu;
 
-	return ioctl(priv->tf_ioctls, SIOCSIFMTU, &priv->tf_ifr);
+	return ioctl(priv->to_ioctls, SIOCSIFMTU, &priv->to_ifr);
 }
 
-static int ti_set_mac_fbsd(struct tif *ti, unsigned char *mac)
+static int ti_set_mac_obsd(struct tif *ti, unsigned char *mac)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
-	struct ifreq *ifr = &priv->tf_ifr;
+	struct tip_obsd *priv = ti_priv(ti);
+	struct ifreq *ifr = &priv->to_ifr;
 
 	ifr->ifr_addr.sa_family = AF_LINK;
 	ifr->ifr_addr.sa_len = 6;
 	memcpy(ifr->ifr_addr.sa_data, mac, 6);
 
-	return ioctl(priv->tf_ioctls, SIOCSIFLLADDR, ifr);
+	return ioctl(priv->to_ioctls, SIOCSIFLLADDR, ifr);
 }
 
-static int ti_set_ip_fbsd(struct tif *ti, struct in_addr *ip)
+static int ti_set_ip_obsd(struct tif *ti, struct in_addr *ip)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 	struct ifaliasreq ifra;
 	struct sockaddr_in *s_in;
 
 	/* assume same size */
 	memset(&ifra, 0, sizeof(ifra));
-	strncpy(ifra.ifra_name, priv->tf_ifr.ifr_name, IFNAMSIZ);
+	strncpy(ifra.ifra_name, priv->to_ifr.ifr_name, IFNAMSIZ);
 
 	s_in = (struct sockaddr_in *) &ifra.ifra_addr;
 	s_in->sin_family = PF_INET;
 	s_in->sin_addr = *ip;
 	s_in->sin_len = sizeof(*s_in);
 
-	return ioctl(priv->tf_ioctls, SIOCAIFADDR, &ifra);
+	return ioctl(priv->to_ioctls, SIOCAIFADDR, &ifra);
 }
 
-static int ti_fd_fbsd(struct tif *ti)
+static int ti_fd_obsd(struct tif *ti)
 {
-	struct tip_fbsd *priv = ti_priv(ti);
+	struct tip_obsd *priv = ti_priv(ti);
 
-	return priv->tf_fd;
+	return priv->to_fd;
 }
 
-static int ti_read_fbsd(struct tif *ti, void *buf, int len)
+static int ti_read_obsd(struct tif *ti, void *buf, int len)
 {
 	return read(ti_fd(ti), buf, len);
 }
 
-static int ti_write_fbsd(struct tif *ti, void *buf, int len)
+static int ti_write_obsd(struct tif *ti, void *buf, int len)
 {
 	return write(ti_fd(ti), buf, len);
 }
 
-static struct tif *ti_open_fbsd(char *iface)
+static struct tif *ti_open_obsd(char *iface)
 {
 	struct tif *ti;
-	struct tip_fbsd *priv;
+	struct tip_obsd *priv;
 	int fd;
 
 	/* setup ti struct */
 	ti = ti_alloc(sizeof(*priv));
 	if (!ti)
 		return NULL;
-	ti->ti_name	= ti_name_fbsd;
-	ti->ti_set_mtu	= ti_set_mtu_fbsd;
-	ti->ti_close	= ti_close_fbsd;
-	ti->ti_fd	= ti_fd_fbsd;
-	ti->ti_read	= ti_read_fbsd;
-	ti->ti_write	= ti_write_fbsd;
-	ti->ti_set_mac	= ti_set_mac_fbsd;
-	ti->ti_set_ip	= ti_set_ip_fbsd;
+	ti->ti_name	= ti_name_obsd;
+	ti->ti_set_mtu	= ti_set_mtu_obsd;
+	ti->ti_close	= ti_close_obsd;
+	ti->ti_fd	= ti_fd_obsd;
+	ti->ti_read	= ti_read_obsd;
+	ti->ti_write	= ti_write_obsd;
+	ti->ti_set_mac	= ti_set_mac_obsd;
+	ti->ti_set_ip	= ti_set_ip_obsd;
 
 	/* setup iface */
-	fd = ti_do_open_fbsd(ti, iface);
+	fd = ti_do_open_obsd(ti, iface);
 	if (fd == -1) {
 		ti_do_free(ti);
 		return NULL;
@@ -199,12 +199,12 @@ static struct tif *ti_open_fbsd(char *iface)
 
 	/* setup private state */
 	priv = ti_priv(ti);
-	priv->tf_fd = fd;
+	priv->to_fd = fd;
 
 	return ti;
 }
 
 struct tif *ti_open(char *iface)
 {
-	return ti_open_fbsd(iface);
+	return ti_open_obsd(iface);
 }
