55ab634c992a2618c37e46b14fd96d977cdbf1c5
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2007-10-27 23:27:33 +0000

    Doesn't compile at the moment
    git-svn-id: http://svn.aircrack-ng.org/branch/1.0-dev@806 28c6078b-6c39-48e3-add9-af49d547ecab

--- contrib/airpcap/airpcap.c
+++ contrib/airpcap/airpcap.c
@@ -1,29 +1,145 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <unistd.h>
-//#include <fcntl.h>
-//#include <wchar.h>
-
 #include <windows.h>
 
 #include <airpcap.h>
+//#include "../../src/pcap.h"
+//#include <pcap.h>
+//#include "../../../developers/WinPcap_Devpack/include/pcap.h"
 
 #include "osdep.h"
 #include "tap-win32/common.h"
 
-const char * DEF_ADAPT_NAME = "\\\\.\\airpcap00";
-const char * ADAPT_HEADER = "\\\\.\\";
-
-PAirpcapHandle device_Handle = NULL;
-CHAR Ebuf[AIRPCAP_ERRBUF_SIZE];
-BYTE* PacketBuffer;
-UINT BytesReceived;
-HANDLE ReadEvent;
+const char * DEFAULT_ADAPT_NAME = "\\\\.\\airpcap00";
+const char * DEVICE_HEADER = "\\\\.\\";
 
 
-#define LASTERR AirpcapGetLastError(device_Handle)
-#define CLOSE_ADAPTER AirpcapClose(device_Handle);
+// New
+pcap_t *winpcap_adapter;
+PAirpcapHandle airpcap_handle;
+// End new
+/*
+// Use PPI later
+#define PPH_PH_VERSION		((u_int8_t)0x00)
+#define	PPI_FIELD_TYPE_802_11_COMMON		((u_int16_t)0x02)
+
+//#pragma pack(push)
+//#pragma pack(1)
+
+typedef struct _PPI_PACKET_HEADER
+{
+	u_int8_t	PphVersion;
+	u_int8_t	PphFlags;
+	u_int16_t	PphLength;
+	u_int32_t	PphDlt;
+}
+PPI_PACKET_HEADER, *PPPI_PACKET_HEADER;
+
+typedef struct _PPI_FIELD_HEADER
+{
+	u_int16_t PfhType;
+	u_int16_t PfhLength;
+}
+PPI_FIELD_HEADER, *PPPI_FIELD_HEADER;
+
+typedef struct _PPI_FIELD_802_11_COMMON
+{
+	u_int64_t	TsfTimer;
+	u_int16_t	Flags;
+	u_int16_t	Rate;
+	u_int16_t	ChannelFrequency;
+	u_int16_t	ChannelFlags;
+	u_int8_t	FhssHopset;
+	u_int8_t	FhssPattern;
+	int8_t		DbmAntSignal;
+	int8_t		DbmAntNoise;
+}
+PPI_FIELD_802_11_COMMON, *PPPI_FIELD_802_11_COMMON;
+//#pragma pack(pop)
+
+
+int ppi_decode(const u_char *p, int caplen, int *hdrlen, int *power)
+{
+	PPPI_PACKET_HEADER pPpiPacketHeader;
+	PPPI_FIELD_HEADER	pFieldHeader;
+	ULONG position = 0;
+
+	// Sanity checks
+	if (caplen < sizeof(*pPpiPacketHeader))
+	{
+		// Packet smaller than the PPI fixed header
+		return( 1 );
+	}
+
+	pPpiPacketHeader = (PPPI_PACKET_HEADER)p;
+
+	*hdrlen = pPpiPacketHeader->PphLength;
+
+	if(caplen < *hdrlen)
+	{
+		// Packet smaller than the PPI fixed header
+		return( 1 );
+	}
+
+	position = sizeof(*pPpiPacketHeader);
+
+	if (pPpiPacketHeader->PphVersion != PPH_PH_VERSION)
+	{
+		fprintf( stderr, "Unknown PPI packet header version (%u)\n", pPpiPacketHeader->PphVersion);
+		return( 1 );
+	}
+
+	do
+	{
+		//
+		// now we suppose to have an 802.11-Common header
+		//
+		if (*hdrlen < sizeof(*pFieldHeader) + position)
+		{
+			break;
+		}
+
+		pFieldHeader = (PPPI_FIELD_HEADER)(p + position);
+		position += sizeof(*pFieldHeader);
+
+		switch(pFieldHeader->PfhType)
+		{
+		case PPI_FIELD_TYPE_802_11_COMMON:
+			if (pFieldHeader->PfhLength != sizeof(PPI_FIELD_802_11_COMMON) || caplen - position < sizeof(PPI_FIELD_802_11_COMMON))
+			{
+				//
+				// the header is bogus, just skip it
+				//
+				fprintf( stderr, "Bogus 802.11-Common Field. Skipping it.\n");
+			}
+			else
+			{
+				PPPI_FIELD_802_11_COMMON pField = (PPPI_FIELD_802_11_COMMON)(p + position);
+
+				if (pField->DbmAntSignal != -128)
+				{
+					*power = (int)pField->DbmAntSignal;
+				}
+				else
+				{
+					*power = 0;
+				}
+			}
+			break;
+
+		default:
+			// we do not know this field. Just print type and length and skip
+			break;
+		}
+
+		position += pFieldHeader->PfhLength;
+	}
+	while(TRUE);
+
+	return( 0 );
+}
+*/
 
 int cygwin_set_mac(unsigned char *mac)
 {
@@ -35,62 +151,84 @@ void cygwin_close(void)
 {
 	// Do not remove monitor mode in case another application forget to set it
 	// (by default, when plugged in, the adapter is set in monitor mode)
-	if (device_Handle != NULL)
-		CLOSE_ADAPTER;
+	if (airpcap_handle != NULL)
+	{
+		AirpcapClose(airpcap_handle);
+	}
 }
 
 int cygwin_get_mac(unsigned char *mac)
 {
    // Don't use the function from Airpcap
+	if (*mac) {}
 
-   return 0;
+	return 0;
 }
 
 int cygwin_sniff(void *buf, int len, struct rx_info *ri)
 {
-	UINT BytesReceived = 0;
-	UINT channel = 0;
+	struct pcap_pkthdr *header;
+	const u_char *pkt_data;
+	int res;
 
-	// Wait for the next packet
-	WaitForSingleObject(ReadEvent, INFINITE);
-
-	// Read a packet
-	if(AirpcapRead(device_Handle, buf, len, &BytesReceived))
+	while( 1 )
 	{
-		if (ri)
-		{
-			// Try getting channel
-			if (AirpcapGetDeviceChannel(device_Handle, &channel))
-			{
-				ri->ri_channel = (int)channel;
-			}
-			// Don't complain if it fails
-		}
-		return (int)BytesReceived;
-	}
+		res = pcap_next_ex( winpcap_adapter, &header, &pkt_data );
 
-	// It failed, return -1
-	printf("Error receiving packets: %s\n", LASTERR);
-	return -1;
+		if( res == 0 )
+		{
+			// timeout elapsed
+			continue;
+		}
+
+		if( res < 0 )
+		{
+			// error
+			return -1;
+		}
+
+		// Good reception
+		memcpy( buf, pkt_data, header->caplen );
+		return( header->caplen );
+	}
 }
+
+
 
 int cygwin_inject(void *buf, int len, struct tx_info *ti)
 {
-	if (buf)
+	if(pcap_sendpacket(winpcap_adapter, buf, len) != 0)
+		return -1;
+
+	return len;
+}
+
+int printErrorCloseAndReturn(const char * err, int retValue)
+{
+	if (err && airpcap_handle)
 	{
-		if (AirpcapWrite(device_Handle, buf, len) == FALSE)
-			return -1;
-		else
-			return len;
+		if (strlen(err))
+		{
+			if (airpcap_handle)
+				fprintf( stderr, err, AirpcapGetLastError(airpcap_handle));
+			else
+				fprintf( stderr, err);
+		}
 	}
+
+	if (airpcap_handle)
+		pcap_close( winpcap_adapter );
+
+    return retValue;
 }
 
 int cygwin_init(char *param)
 {
-	char * adapter;
+	char * iface;
+    char errbuf[65535];
 
-	adapter = (char *)calloc(1, strlen(param) + strlen(ADAPT_HEADER) +1);
-	strcpy (adapter, DEF_ADAPT_NAME);
+	iface = (char *)calloc(1, strlen(param) + strlen(DEVICE_HEADER) +1);
+	strcpy (iface, DEFAULT_ADAPT_NAME);
 
 	if (param)
 	{
@@ -98,71 +236,78 @@ int cygwin_init(char *param)
 		if (strlen(param) > 0)
 		{
 			// Make sure the adapter name contains the '\\.\' at its begining
-			if (strstr(param, ADAPT_HEADER) == NULL)
+			memset(iface, 0, strlen(param) + strlen(DEVICE_HEADER) +1);
+
+			if (strstr(param, DEVICE_HEADER) == NULL)
 			{
 				// Not found, add it
-				memset(adapter, 0, strlen(param) + strlen(ADAPT_HEADER) +1);
-				strcpy(adapter, ADAPT_HEADER);
-				strcat(adapter, param);
+
+				strcpy(iface, DEVICE_HEADER);
+				strcat(iface, param);
 			}
 			else
 			{
 				// Already contains the adapter header
-				free(adapter);
-				adapter = param;
+				strcpy(iface, param);
 			}
 		}
 	}
 
-	// Open the adapter
-	device_Handle = AirpcapOpen(adapter, Ebuf);
+    /* Open the adapter with WinPcap */
+    if((winpcap_adapter = pcap_open_live(iface,
+        65536,
+        1,
+        1000,
+        errbuf)) == NULL)
+    {
+        fprintf( stderr, "Error opening adapter %s with winpcap (%s)\n", iface, errbuf);
+        return( -1 );
+    }
 
-	if (device_Handle == NULL  )
+    free(iface);
+
+    /* Get the airpcap handle so we can change wireless-specific settings */
+    airpcap_handle = pcap_get_airpcap_handle(winpcap_adapter);
+
+    if(airpcap_handle == NULL)
+    {
+        fprintf( stderr, "This adapter doesn't have wireless extensions. Quitting\n");
+        pcap_close( winpcap_adapter );
+        return( -1 );
+    }
+
+    /* Tell the adapter that the packets we'll send and receive don't include the FCS */
+    if(!AirpcapSetFcsPresence(airpcap_handle, FALSE))
+    {
+		return printErrorCloseAndReturn("Error setting FCS presence: %s\n"
+										airpcap_handle,
+										-1);
+    }
+
+    /* Set the link layer to bare 802.11 */
+    if(!AirpcapSetLinkType(airpcap_handle, AIRPCAP_LT_802_11))
+    {
+		return printErrorCloseAndReturn("Error setting the link type: %s\n"
+										airpcap_handle,
+										-1);
+    }
+
+    /* Accept correct frames only */
+	if( !AirpcapSetFcsValidation(airpcap_handle, AIRPCAP_VT_ACCEPT_CORRECT_FRAMES) )
 	{
-		printf("Error opening adapter: %s\n", LASTERR);
-		return -1;
+		return printErrorCloseAndReturn("Error setting FCS validation: %s\n"
+										airpcap_handle,
+										-1);
 	}
 
+    /* Set a low mintocopy for better responsiveness */
+    if(!AirpcapSetMinToCopy(airpcap_handle, 1))
+    {
+		return printErrorCloseAndReturn("Error setting MinToCopy: %s\n"
+										airpcap_handle,
+										-1);
+    }
 
-	// Set monitor mode
-	if(!AirpcapSetMonitorMode(device_Handle, TRUE))
-	{
-		printf("Error turning monitor mode on: %s\n", adapter);
-		return -1;
-	}
-
-
-	// Set the link layer to 802.11
-	if(!AirpcapSetLinkType(device_Handle, AIRPCAP_LT_802_11))
-	{
-		printf("Error setting the link layer: %s\n", LASTERR);
-		return -1;
-	}
-
-	// Remove FCS
-	if (AirpcapSetFcsPresence(device_Handle, FALSE) == FALSE)
-	{
-		printf("Error disabling FCS presence. Error: %s\n", LASTERR);
-		return -1;
-	}
-
-	// Only get valid frames
-	if (AirpcapSetFcsValidation(device_Handle,
-		//AirpcapValidationType.AIRPCAP_VT_ACCEPT_CORRECT_FRAMES) == FALSE)
-		AIRPCAP_VT_ACCEPT_CORRECT_FRAMES) == FALSE)
-	{
-		printf("Error setting FCS validation. Error: %s\n", LASTERR);
-		return -1;
-	}
-
-	// Add read event
-	if (AirpcapGetReadEvent(device_Handle, &ReadEvent) == FALSE)
-	{
-		printf("Error getting the read event: %s\n", LASTERR);
-		return -1;
-	}
-
-	AirpcapSetMinToCopy(device_Handle, 1);
 
 	return 0;
 }
@@ -173,9 +318,9 @@ int cygwin_set_chan(int chan)
 	if (chan <= 0)
 		return -1;
 
-	if(!AirpcapSetDeviceChannel(device_Handle, chan))
+	if(!AirpcapSetDeviceChannel(airpcap_handle, chan))
 	{
-		printf("Error setting the channel to %d: %s\n", chan, LASTERR);
+		printf("Error setting the channel to %d: %s\n", chan, AirpcapGetLastError(airpcap_handle));
 		return -1;
 	}
 
