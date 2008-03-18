17d021061470a23979326096b5510786008f28f6
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2008-03-18 12:23:36 +0000

    Added copyright and GPL (copyright date may have to be adjusted).
    git-svn-id: http://svn.aircrack-ng.org/trunk@1007 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/uniqueiv.c
+++ src/uniqueiv.c
@@ -1,6 +1,39 @@
 /*
- *  IV uniqueness detection method, as designed by Stanislaw Pusep:
+ *  IV uniqueness detection method.
  *
+ *  Copyright (C) 2004-2008 Stanislaw Pusep:
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  In addition, as a special exception, the copyright holders give
+ *  permission to link the code of portions of this program with the
+ *  OpenSSL library under certain conditions as described in each
+ *  individual source file, and distribute linked combinations
+ *  including the two.
+ *  You must obey the GNU General Public License in all respects
+ *  for all of the code used other than OpenSSL. *  If you modify
+ *  file(s) with this exception, you may extend this exception to your
+ *  version of the file(s), but you are not obligated to do so. *  If you
+ *  do not wish to do so, delete this exception statement from your
+ *  version. *  If you delete this exception statement from all source
+ *  files in the program, then also delete it here.
+ */
+
+
+/*
  *  Each IV byte is stored in corresponding "level". We have 3 levels with
  *  IV[2] as root index (level 0), IV[1] and IV[2] as level 2 and level 1
  *  indices respectively. Space required to allocate all data is at maximum
