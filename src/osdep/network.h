816bd18887126711b5bd3161d5d47f5533ddc243
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2008-04-11 19:07:09 +0000

    Fixed cygwin compilation (Closes: #414).
    git-svn-id: http://svn.aircrack-ng.org/trunk@1025 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/osdep/network.h
+++ src/osdep/network.h
@@ -37,4 +37,59 @@ extern int net_send(int s, int command, void *arg, int len);
 extern int net_read_exact(int s, void *arg, int len);
 extern int net_get(int s, void *arg, int *len);
 
+
+
+#if defined(__CYGWIN32__)
+
+	// Hack for windows (but will only work on 2000 and up).
+	#ifndef MSG_WAITALL
+		#define MSG_WAITALL    0x08
+	#endif
+
+
+	#include <asm/byteorder.h>
+	#include <unistd.h>
+
+	#define ___my_be16cpu(x) \
+	((u_int16_t)( \
+			(((u_int16_t)(x) & (u_int16_t)0x00ffU) << 8) | \
+			(((u_int16_t)(x) & (u_int16_t)0xff00U) >> 8) ))
+
+	#define ___my_be32cpu(x) \
+	((u_int32_t)( \
+			(((u_int32_t)(x) & (u_int32_t)0x000000ffUL) << 24) | \
+			(((u_int32_t)(x) & (u_int32_t)0x0000ff00UL) <<  8) | \
+			(((u_int32_t)(x) & (u_int32_t)0x00ff0000UL) >>  8) | \
+			(((u_int32_t)(x) & (u_int32_t)0xff000000UL) >> 24) ))
+
+	#define ___my_be64cpu(x) \
+	((u_int64_t)( \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x00000000000000ffULL) << 56) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x000000000000ff00ULL) << 40) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x0000000000ff0000ULL) << 24) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x00000000ff000000ULL) <<  8) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x000000ff00000000ULL) >>  8) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x0000ff0000000000ULL) >> 24) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x00ff000000000000ULL) >> 40) | \
+			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0xff00000000000000ULL) >> 56) ))
+
+
+
+	#define __be64_to_cpu(x) ___my_be64cpu(x)
+	#define __be32_to_cpu(x) ___my_be32cpu(x)
+	#define __be16_to_cpu(x) ___my_be16cpu(x)
+	#define __cpu_to_be64(x) ___my_be64cpu(x)
+	#define __cpu_to_be32(x) ___my_be32cpu(x)
+	#define __cpu_to_be16(x) ___my_be16cpu(x)
+	#define __le64_to_cpu(x) (x)
+	#define __le32_to_cpu(x) (x)
+	#define __le16_to_cpu(x) (x)
+	#define __cpu_to_le64(x) (x)
+	#define __cpu_to_le32(x) (x)
+	#define __cpu_to_le16(x) (x)
+
+
+
+#endif /* __CYGWIN32__ */
+
 #endif /* __AIRCRACK_NG_OSEDEP_NETWORK_H__ */
