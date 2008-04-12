619754a2cde3a475ca9927c96ed27d17be599ed4
Daouid <daouid@gmail.com>
2008-04-12 20:32:39 +0000

    -fixes and updates (still shitty script)
    git-svn-id: http://svn.aircrack-ng.org/branch/airoscript@1027 28c6078b-6c39-48e3-add9-af49d547ecab

--- airopdate.sh
+++ airopdate.sh
@@ -2,7 +2,7 @@
 
 # Program:	Airopdate                                                          
 # Authors:	Base Code by Daouid
-# Date:	        15.05.2007
+# Date:	        10.04.2008
 
 #The path where the data is stored (FOLDER MUST EXIST !)
 DUMP_PATH="/wifi"
@@ -10,17 +10,15 @@ DUMP_PATH="/wifi"
 # leave this alone (if you edit this, it will screw up the menu)
 CHOICES="1 2 3 4 5"
 
-
 function chooseversion {
 while true; do
   clear
   echo "#######################################"
   echo "###   Select aircrack-ng version    ###"
   echo "###                                 ###"
-  echo "###   1) Latest 0.9 branch          ###"
-  echo "###   2) Latest 1.0-dev             ###"
+  echo "###   1) Stable 0.9.3               ###"
+  echo "###   2) Latest 1.0-beta2           ###"
   echo "###   3) Specific revision          ###"
-  echo "###   4) Latest Stable              ###"
   echo "###                                 ###"
   echo "#######################################"
   read yn
@@ -28,14 +26,13 @@ while true; do
     1 ) latest09 ; break ;;
     2 ) latest10 ; break ;;
     3 ) svnrev ; break ;;
-    4 ) stable ; break ;;
     * ) echo "unknown response. Try again" ;;
   esac
 done 
 }
 function latest09 {
-svn co http://trac.aircrack-ng.org/svn/trunk/ aircrack-ng-svn
-cd aircrack-ng-svn
+svn co http://trac.aircrack-ng.org/svn/tags/0.9.3/ aircrack-ng-0.9.3
+cd aircrack-ng-0.9.3
 make clean
 make uninstall
 make
@@ -43,8 +40,8 @@ make install
 cd $DUMP_PATH
 }
 function latest10 {
-svn co http://trac.aircrack-ng.org/svn/branch/1.0-dev aircrack-ng-dev
-cd aircrack-ng-dev
+svn co http://trac.aircrack-ng.org/svn/trunk/ aircrack-ng-1.02
+cd aircrack-ng-1.02
 make clean
 make uninstall
 make
@@ -60,17 +57,7 @@ echo You typed: $rev
 set -- ${rev}
 
 svn co -r $rev http://trac.aircrack-ng.org/svn/trunk/ aircrack-ng-r"$rev"
-cd aircrack-ng-rev
-make clean
-make uninstall
-make
-make install
-cd $DUMP_PATH
-}
-
-function stable {
-svn co http://trac.aircrack-ng.org/svn/tags/0.9 aircrack-ng-stable
-cd aircrack-ng-stable
+cd aircrack-ng-r"$rev"
 make clean
 make uninstall
 make
@@ -90,7 +77,8 @@ while true; do
   clear
   echo "#######################################"
   echo "###  Select driver/chipset version  ###"
-  echo "###                                 ###"
+  echo "###  Currently outdated use         ###"
+  echo "###  airdriver-ng instead           ###"
   echo "###   1) ASPj's rt2570 drivers      ###"
   echo "###   2) rt2500                     ###"
   echo "###   3) Madwifi-ng                 ###"
@@ -197,9 +185,9 @@ cd $DUMP_PATH
 function rt2570 {
 ifconfig rausb0 down
 rmmod rt2570
-wget http://homepages.tu-darmstadt.de/~p_larbig/wlan/rt2570-k2wrlz-1.6.0.tar.bz2
-tar -xvjf rt2570-k2wrlz-1.6.0.tar.bz2
-cd rt2570-k2wrlz-1.6.0/Module
+wget http://homepages.tu-darmstadt.de/~p_larbig/wlan/rt2570-k2wrlz-1.6.1.tar.bz2
+tar -xvjf rt2570-k2wrlz-1.6.1.tar.bz2
+cd rt2570-k2wrlz-1.6.1/Module
 make && make install
 modprobe rt2570
 cd $DUMP_PATH
