2c6e2a1738b6541cdc02c5dd32ccc3e0ae8c5650
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2008-10-18 22:43:57 +0000

    Added function that returns the number of CPU on the computer (more reliable than the existing code in aircrack-ng.c for recent CPU).
    git-svn-id: http://svn.aircrack-ng.org/trunk@1179 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/common.c
+++ src/common.c
@@ -71,6 +71,53 @@ char * getVersion(char * progname, int maj, int min, int submin, int svnrev, int
 	return temp;
 }
 
+// Return the number of cpu. If detection fails, it will return -1;
+int get_nb_cpus()
+{
+		// Optmization for windows: use GetSystemInfo()
+        char * s, * pos;
+        FILE * f;
+        int number = -1;
+
+		// Reading /proc/cpuinfo is more reliable on current CPUs,
+		// so put it first and try the old method if this one fails
+        f = fopen("/proc/cpuinfo", "r");
+        if (f != NULL) {
+				s = (char *)calloc(1, 81);
+				if (s != NULL) {
+					// Get the latest value of "processor" element
+					// and increment it by 1 and it that value
+					// will be the number of CPU.
+					number = -2;
+					while (fgets(s, 80, f) != NULL) {
+							pos = strstr(s, "processor");
+							if (pos == s) {
+									pos = strchr(s, ':');
+									number = atoi(pos + 1);
+							}
+					}
+					fclose(f);
+
+					++number;
+				}
+        }
+
+        #ifdef _SC_NPROCESSORS_ONLN
+        // Try the usual method if _SC_NPROCESSORS_ONLN exist
+        if (number == -1) {
+
+			number   = sysconf(_SC_NPROCESSORS_ONLN);
+			/* Fails on some archs */
+			if (number < 1) {
+				number = -1;
+			}
+        }
+        #endif
+
+        return number;
+}
+
+
 //compares two MACs
 int maccmp(unsigned char *mac1, unsigned char *mac2)
 {
