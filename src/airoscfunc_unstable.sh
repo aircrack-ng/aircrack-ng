f0e90e743680d8b992da3c5f59ad6de51892ae0a
David Francos <xayon@xayon.net>
2008-11-15 19:11:04 +0000

    Variables can still be wrong, modified if $Host_SSID eq "" to a case in wich wlan kind is detected.
    git-svn-id: http://svn.aircrack-ng.org/branch/airoscript@1225 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airoscfunc_unstable.sh
+++ src/airoscfunc_unstable.sh
@@ -3,6 +3,12 @@
 # Requires: wlandecrypter 
 # By David Francos (XayOn) <yo.orco@gmail.com>
 echo -e "\tUnstable functions from airoscript loaded,\n\tif you don't want this, set UNSTABLE=0 in\n\tconfig file"
+function doitwld {
+	airodump-ng -w $CAPFILE  $OPT $CHANNEL $WIFI
+	clear 
+	wlandecrypter $Host_MAC $Host_SSID $DICFILE 
+	aircrack-ng -b $Host_MAC -w $DICFILE $CAPFILE 
+}
 
 function wld {
 # Here goes, if no host_mac is present, ask for it. Hope it's OK.	
@@ -26,17 +32,17 @@ function wld {
 			echo -e "I'm going to launch airodump now, when you\n get sufficent iv's, stop it" 
 
 			sleep 2 
-			if [ "$HOST_SSID" -ne "" ] # FIXME replace this for a case with all the supported types or something
-			then
-				airodump-ng -w $CAPFILE  $OPT $CHANNEL $WIFI
-				clear 
-				wlandecrypter $HOST_MAC $HOST_SSID $DICFILE 
-				aircrack-ng -b $HOST_MAC -w $DICFILE $CAPFILE 
-			else
-				echo "No valid ssid detected, your target must be\n supported by wlandecrypter"
-			fi
-	
-	else
+			case $Host_SSID in
+				WLAN_[1-9][1-9] )
+					echo "Your wifi is the form WLAN_XX so I'll try it"
+					doitwld
+					;;
+				*)
+					echo "Sorry, your selected wlan is not supported"
+					;;
+			esac
+				
+
 		clear
 		echo -e "Error: You must select a client before performing this cracking.\n"
 	fi
