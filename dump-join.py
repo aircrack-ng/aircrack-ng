987764c8cf73c047e99ccd05760ca130698a12e2
Ben Smith <thex1le@remote-exploit.org>
2008-11-23 08:49:21 +0000

    Updated dumpjoin.py to allow more then two input files for fast joining
    git-svn-id: http://svn.aircrack-ng.org/branch/airgraph-ng@1228 28c6078b-6c39-48e3-add9-af49d547ecab

--- dump-join.py
+++ dump-join.py
@@ -3,7 +3,11 @@
 # this script is a total hack it works and ill clean it up later
 import sys,getopt, optparse, pdb, re
 def raw_lines(file):
-        raw_lines = open(file, "r")
+        try:
+		raw_lines = open(file, "r")
+	except Exception:
+		print "Failed to open ",file,". Do you have the file name correct?"
+		sys.exit(1)
         Rlines = raw_lines.readlines() 
        	return Rlines
 
@@ -36,7 +40,7 @@ def join_write(data,name):
          		file.write(line + '\n')
 	file.close()
 def showBanner():
-	print "Airodump Joiner\nJoin Two Airodump CSV Files\n\n\t-i\tInput Files [ foo_name_1 foo_name_2 ] \n\t-o\tOutput File\n"
+	print "Airodump Joiner\nJoin Two Airodump CSV Files\n\n\t-i\tInput Files [ foo_name_1 foo_name_2 foo_name_3 .....] \n\t-o\tOutput File\n"
 
 def file_pool(files):
 	AP = []
@@ -65,23 +69,23 @@ if  __name__ == "__main__":
                 showBanner()
                 sys.exit(1)
 
-	parser = optparse.OptionParser("usage: %prog [options] arg1 arg2")
-	parser.add_option("-o", "--output",  dest="output",help="output file to write to")
-	parser.add_option("-i", "--file", dest="filename", nargs=2 ,help="Input files to read data from requires two arguments")
+	parser = optparse.OptionParser("usage: %prog [options] arg1 arg2 arg3 .....")
+	parser.add_option("-o", "--output",  dest="output",nargs=1, help="output file to write to")
+	parser.add_option("-i", "--file", dest="filename", nargs=2 ,help="Input files to read data from requires at least two arguments")
 	
 	(options, args) = parser.parse_args()
 	filenames = options.filename
 	outfile = options.output
-	#pdb.set_trace()
 	if outfile == None:
 		print "You must provide a file name to write out to. IE... -o foo.txt\n"
 		showBanner()
 		sys.exit(1)
 	elif filenames == None:
-		print "You must provide two file names to join. IE... -i foo1.txt foo2.txt\n"
+		print "You must provide at least two file names to join. IE... -i foo1.txt foo2.txt\n"
 		showBanner()
 		sys.exit(1)
-	
+	for file_name in args:
+		filenames += (file_name,)	
 	return_var = file_pool(filenames)
 	return_var = join_write(return_var,outfile)
 
