7583e57b22a86761176a4290773c550ece4474cc
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2009-06-14 00:43:15 +0000

    Improved compatibility.h.
    git-svn-id: http://svn.aircrack-ng.org/trunk@1571 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/osdep/compatibility.h
+++ src/osdep/compatibility.h
@@ -1,60 +1,40 @@
 /*
- * Copyright (C) 2009 Thomas d'Otreppe de Bouvette
+ *  Compatibility header
  *
- * Copyright (C) Jan 2006 Mellanox Technologies Ltd. All rights reserved.
+ *  Copyright (C) 2009 Thomas d'Otreppe
  *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * OpenIB.org BSD license below:
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *  compatibility.h - Miscellaneous systems compatibility issues
- *
- *  Version: $Id: compatibility.h 2752 2006-01-19 14:40:17Z mst $
- *
+ *  In addition, as a special exception, the copyright holders give
+ *  permission to link the code of portions of this program with the
+ *  OpenSSL library under certain conditions as described in each
+ *  individual source file, and distribute linked combinations
+ *  including the two.
+ *  You must obey the GNU General Public License in all respects
+ *  for all of the code used other than OpenSSL. *  If you modify
+ *  file(s) with this exception, you may extend this exception to your
+ *  version of the file(s), but you are not obligated to do so. *  If you
+ *  do not wish to do so, delete this exception statement from your
+ *  version. *  If you delete this exception statement from all source
+ *  files in the program, then also delete it here.
  */
-
-/*
-TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macros
-      only once for all architectures with maybe a few exceptions.
-      And thus only the includes for each OS will be left.
-*/
 
 #ifndef _AIRCRACK_NG__COMPATIBILITY_H_
 #define _AIRCRACK_NG_COMPATIBILITY_H_
 
-	#if defined(__ia64__) || defined(__x86_64__)
-	 #define U64L       "l"
-	#else
-	 #define U64L       "ll"
-	#endif
-
-	/*
-	 * Only for architectures which can't do swab by themselves
-	 */
 	#define ___my_swab16(x) \
 	((u_int16_t)( \
 			(((u_int16_t)(x) & (u_int16_t)0x00ffU) << 8) | \
@@ -76,74 +56,66 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0x00ff000000000000ULL) >> 40) | \
 			(u_int64_t)(((u_int64_t)(x) & (u_int64_t)0xff00000000000000ULL) >> 56) ))
 
+
 	/*
 	 * Linux
 	 */
 	#if defined(linux)
-	// #include <asm/byteorder.h>
-	#include <endian.h>
-	#include <unistd.h>
+		#include <endian.h>
+		#include <unistd.h>
 
-	#define __be64_to_cpu(x) ___my_swab64(x)
-	#define __be32_to_cpu(x) ___my_swab32(x)
-	#define __be16_to_cpu(x) ___my_swab16(x)
-	#define __cpu_to_be64(x) ___my_swab64(x)
-	#define __cpu_to_be32(x) ___my_swab32(x)
-	#define __cpu_to_be16(x) ___my_swab16(x)
-	#define __le64_to_cpu(x) (x)
-	#define __le32_to_cpu(x) (x)
-	#define __le16_to_cpu(x) (x)
-	#define __cpu_to_le64(x) (x)
-	#define __cpu_to_le32(x) (x)
-	#define __cpu_to_le16(x) (x)
+		#if BYTE_ORDER == BIG_ENDIAN
 
-	#define be64_to_cpu(x) __be64_to_cpu(x)
-	#define be32_to_cpu(x) __be32_to_cpu(x)
-	#define be16_to_cpu(x) __be16_to_cpu(x)
-	#define cpu_to_be64(x) __cpu_to_be64(x)
-	#define cpu_to_be32(x) __cpu_to_be32(x)
-	#define cpu_to_be16(x) __cpu_to_be16(x)
-	#define le64_to_cpu(x) (x)
-	#define le32_to_cpu(x) (x)
-	#define le16_to_cpu(x) (x)
-	#define cpu_to_le64(x) (x)
-	#define cpu_to_le32(x) (x)
-	#define cpu_to_le16(x) (x)
+			#define __be64_to_cpu(x) (x)
+			#define __be32_to_cpu(x) (x)
+			#define __be16_to_cpu(x) (x)
+			#define __cpu_to_be64(x) (x)
+			#define __cpu_to_be32(x) (x)
+			#define __cpu_to_be16(x) (x)
+			#define __le64_to_cpu(x) ___my_swab64(x)
+			#define __le32_to_cpu(x) ___my_swab32(x)
+			#define __le16_to_cpu(x) ___my_swab16(x)
+			#define __cpu_to_le64(x) ___my_swab64(x)
+			#define __cpu_to_le32(x) ___my_swab32(x)
+			#define __cpu_to_le16(x) ___my_swab16(x)
+
+		#elif BYTE_ORDER == LITTLE_ENDIAN
+
+			#define __be64_to_cpu(x) ___my_swab64(x)
+			#define __be32_to_cpu(x) ___my_swab32(x)
+			#define __be16_to_cpu(x) ___my_swab16(x)
+			#define __cpu_to_be64(x) ___my_swab64(x)
+			#define __cpu_to_be32(x) ___my_swab32(x)
+			#define __cpu_to_be16(x) ___my_swab16(x)
+			#define __le64_to_cpu(x) (x)
+			#define __le32_to_cpu(x) (x)
+			#define __le16_to_cpu(x) (x)
+			#define __cpu_to_le64(x) (x)
+			#define __cpu_to_le32(x) (x)
+			#define __cpu_to_le16(x) (x)
+		#endif
 
 	#endif
 
 	/*
-	 * Windows (CYGWIN)
+	 * Cygwin
 	 */
 	#if defined(__CYGWIN32__)
-	#include <asm/byteorder.h>
-	#include <unistd.h>
+		#include <asm/byteorder.h>
+		#include <unistd.h>
 
-	#define __be64_to_cpu(x) ___my_swab64(x)
-	#define __be32_to_cpu(x) ___my_swab32(x)
-	#define __be16_to_cpu(x) ___my_swab16(x)
-	#define __cpu_to_be64(x) ___my_swab64(x)
-	#define __cpu_to_be32(x) ___my_swab32(x)
-	#define __cpu_to_be16(x) ___my_swab16(x)
-	#define __le64_to_cpu(x) (x)
-	#define __le32_to_cpu(x) (x)
-	#define __le16_to_cpu(x) (x)
-	#define __cpu_to_le64(x) (x)
-	#define __cpu_to_le32(x) (x)
-	#define __cpu_to_le16(x) (x)
-
-	#define be64_to_cpu(x) __be64_to_cpu(x)
-	#define be32_to_cpu(x) __be32_to_cpu(x)
-	#define be16_to_cpu(x) __be16_to_cpu(x)
-	#define cpu_to_be64(x) __cpu_to_be64(x)
-	#define cpu_to_be32(x) __cpu_to_be32(x)
-	#define cpu_to_be16(x) __cpu_to_be16(x)
-	#define le64_to_cpu(x) (x)
-	#define le32_to_cpu(x) (x)
-	#define le16_to_cpu(x) (x)
-	#define cpu_to_le64(x) (x)
-	#define cpu_to_le32(x) (x)
-	#define cpu_to_le16(x) (x)
+		#define __be64_to_cpu(x) ___my_swab64(x)
+		#define __be32_to_cpu(x) ___my_swab32(x)
+		#define __be16_to_cpu(x) ___my_swab16(x)
+		#define __cpu_to_be64(x) ___my_swab64(x)
+		#define __cpu_to_be32(x) ___my_swab32(x)
+		#define __cpu_to_be16(x) ___my_swab16(x)
+		#define __le64_to_cpu(x) (x)
+		#define __le32_to_cpu(x) (x)
+		#define __le16_to_cpu(x) (x)
+		#define __cpu_to_le64(x) (x)
+		#define __cpu_to_le32(x) (x)
+		#define __cpu_to_le16(x) (x)
 
 	#endif
 
@@ -152,101 +124,48 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 	 */
 	#if defined(__WIN__)
 
+		#include <io.h>
 
-	#include <io.h>
-
-
-	#define __be64_to_cpu(x) ___my_swab64(x)
-	#define __be32_to_cpu(x) ___my_swab32(x)
-	#define __be16_to_cpu(x) ___my_swab16(x)
-	#define __cpu_to_be64(x) ___my_swab64(x)
-	#define __cpu_to_be32(x) ___my_swab32(x)
-	#define __cpu_to_be16(x) ___my_swab16(x)
-	#define __le64_to_cpu(x) (x)
-	#define __le32_to_cpu(x) (x)
-	#define __le16_to_cpu(x) (x)
-	#define __cpu_to_le64(x) (x)
-	#define __cpu_to_le32(x) (x)
-	#define __cpu_to_le16(x) (x)
-
-	#define be64_to_cpu(x) __be64_to_cpu(x)
-	#define be32_to_cpu(x) __be32_to_cpu(x)
-	#define be16_to_cpu(x) __be16_to_cpu(x)
-	#define cpu_to_be64(x) __cpu_to_be64(x)
-	#define cpu_to_be32(x) __cpu_to_be32(x)
-	#define cpu_to_be16(x) __cpu_to_be16(x)
-	#define le64_to_cpu(x) (x)
-	#define le32_to_cpu(x) (x)
-	#define le16_to_cpu(x) (x)
-	#define cpu_to_le64(x) (x)
-	#define cpu_to_le32(x) (x)
-	#define cpu_to_le16(x) (x)
-
-	typedef unsigned __int8  u_int8_t;
-	typedef __int8           int8_t;
-	typedef unsigned __int16 u_int16_t;
-	typedef __int16          int16_t;
-	typedef unsigned __int32 u_int32_t;
-	typedef __int32          int32_t;
-	typedef unsigned __int64 u_int64_t;
-	typedef __int64          int64_t;
-
-	#define strcasecmp    _stricmp
-	#define strtoll       _strtoi64
-	#define strtoull      _strtoui64
-	#define vsnprintf     _vsnprintf
-
-	#else
-
-	#include <sys/time.h>
-	#include <strings.h>
-
+		#define __be64_to_cpu(x) ___my_swab64(x)
+		#define __be32_to_cpu(x) ___my_swab32(x)
+		#define __be16_to_cpu(x) ___my_swab16(x)
+		#define __cpu_to_be64(x) ___my_swab64(x)
+		#define __cpu_to_be32(x) ___my_swab32(x)
+		#define __cpu_to_be16(x) ___my_swab16(x)
+		#define __le64_to_cpu(x) (x)
+		#define __le32_to_cpu(x) (x)
+		#define __le16_to_cpu(x) (x)
+		#define __cpu_to_le64(x) (x)
+		#define __cpu_to_le32(x) (x)
+		#define __cpu_to_le16(x) (x)
 	#endif
-
-
 
 	/*
 	 * MAC (Darwin)
 	 */
 	#if defined(__APPLE_CC__)
-	#include <architecture/byte_order.h>
+		#include <architecture/byte_order.h>
 
-	#define __swab64(x)      NXSwapLongLong(x)
-	#define __swab32(x)      NXSwapLong(x)
-	#define __swab16(x)      NXSwapShort(x)
-	#define __be64_to_cpu(x) NXSwapBigLongLongToHost(x)
-	#define __be32_to_cpu(x) NXSwapBigLongToHost(x)
-	#define __be16_to_cpu(x) NXSwapBigShortToHost(x)
-	#define __le64_to_cpu(x) NXSwapLittleLongLongToHost(x)
-	#define __le32_to_cpu(x) NXSwapLittleLongToHost(x)
-	#define __le16_to_cpu(x) NXSwapLittleShortToHost(x)
-	#define __cpu_to_be64(x) NXSwapHostLongLongToBig(x)
-	#define __cpu_to_be32(x) NXSwapHostLongToBig(x)
-	#define __cpu_to_be16(x) NXSwapHostShortToBig(x)
-	#define __cpu_to_le64(x) NXSwapHostLongLongToLittle(x)
-	#define __cpu_to_le32(x) NXSwapHostLongToLittle(x)
-	#define __cpu_to_le16(x) NXSwapHostShortToLittle(x)
+		#define __swab64(x)      NXSwapLongLong(x)
+		#define __swab32(x)      NXSwapLong(x)
+		#define __swab16(x)      NXSwapShort(x)
+		#define __be64_to_cpu(x) NXSwapBigLongLongToHost(x)
+		#define __be32_to_cpu(x) NXSwapBigLongToHost(x)
+		#define __be16_to_cpu(x) NXSwapBigShortToHost(x)
+		#define __le64_to_cpu(x) NXSwapLittleLongLongToHost(x)
+		#define __le32_to_cpu(x) NXSwapLittleLongToHost(x)
+		#define __le16_to_cpu(x) NXSwapLittleShortToHost(x)
+		#define __cpu_to_be64(x) NXSwapHostLongLongToBig(x)
+		#define __cpu_to_be32(x) NXSwapHostLongToBig(x)
+		#define __cpu_to_be16(x) NXSwapHostShortToBig(x)
+		#define __cpu_to_le64(x) NXSwapHostLongLongToLittle(x)
+		#define __cpu_to_le32(x) NXSwapHostLongToLittle(x)
+		#define __cpu_to_le16(x) NXSwapHostShortToLittle(x)
 
-	#define __LITTLE_ENDIAN 1234
-	#define __BIG_ENDIAN    4321
-	#define __PDP_ENDIAN    3412
-	#define __BYTE_ORDER    __BIG_ENDIAN
-
-	#define swab64(x) __swab64(x)
-	#define swab32(x) __swab32(x)
-	#define swab16(x) __swab16(x)
-	#define be64_to_cpu(x) __be64_to_cpu(x)
-	#define be32_to_cpu(x) __be32_to_cpu(x)
-	#define be16_to_cpu(x) __be16_to_cpu(x)
-	#define le64_to_cpu(x) __le64_to_cpu(x)
-	#define le32_to_cpu(x) __le32_to_cpu(x)
-	#define le16_to_cpu(x) __le16_to_cpu(x)
-	#define cpu_to_be64(x) __cpu_to_be64(x)
-	#define cpu_to_be32(x) __cpu_to_be32(x)
-	#define cpu_to_be16(x) __cpu_to_be16(x)
-	#define cpu_to_le64(x) __cpu_to_le64(x)
-	#define cpu_to_le32(x) __cpu_to_le32(x)
-	#define cpu_to_le16(x) __cpu_to_le16(x)
+		#define __LITTLE_ENDIAN 1234
+		#define __BIG_ENDIAN    4321
+		#define __PDP_ENDIAN    3412
+		#define __BYTE_ORDER    __BIG_ENDIAN
 
 	#endif
 
@@ -259,39 +178,25 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 	#include <sys/types.h>
 	#include <unistd.h>
 
-	#define __be64_to_cpu(x) (x)
-	#define __be32_to_cpu(x) (x)
-	#define __be16_to_cpu(x) (x)
-	#define __cpu_to_be64(x) (x)
-	#define __cpu_to_be32(x) (x)
-	#define __cpu_to_be16(x) (x)
-	#define __le64_to_cpu(x) ___my_swab64(x)
-	#define __le32_to_cpu(x) ___my_swab32(x)
-	#define __le16_to_cpu(x) ___my_swab16(x)
-	#define __cpu_to_le64(x) ___my_swab64(x)
-	#define __cpu_to_le32(x) ___my_swab32(x)
-	#define __cpu_to_le16(x) ___my_swab16(x)
+		#define __be64_to_cpu(x) (x)
+		#define __be32_to_cpu(x) (x)
+		#define __be16_to_cpu(x) (x)
+		#define __cpu_to_be64(x) (x)
+		#define __cpu_to_be32(x) (x)
+		#define __cpu_to_be16(x) (x)
+		#define __le64_to_cpu(x) ___my_swab64(x)
+		#define __le32_to_cpu(x) ___my_swab32(x)
+		#define __le16_to_cpu(x) ___my_swab16(x)
+		#define __cpu_to_le64(x) ___my_swab64(x)
+		#define __cpu_to_le32(x) ___my_swab32(x)
+		#define __cpu_to_le16(x) ___my_swab16(x)
 
-	typedef uint64_t u_int64_t;
-	typedef uint32_t u_int32_t;
-	typedef uint16_t u_int16_t;
-	typedef uint8_t  u_int8_t;
-
-	#define be64_to_cpu(x) __be64_to_cpu(x)
-	#define be32_to_cpu(x) __be32_to_cpu(x)
-	#define be16_to_cpu(x) __be16_to_cpu(x)
-	#define cpu_to_be64(x) __cpu_to_be64(x)
-	#define cpu_to_be32(x) __cpu_to_be32(x)
-	#define cpu_to_be16(x) __cpu_to_be16(x)
-	#define le64_to_cpu(x) (x)
-	#define le32_to_cpu(x) (x)
-	#define le16_to_cpu(x) (x)
-	#define cpu_to_le64(x) (x)
-	#define cpu_to_le32(x) (x)
-	#define cpu_to_le16(x) (x)
+		typedef uint64_t u_int64_t;
+		typedef uint32_t u_int32_t;
+		typedef uint16_t u_int16_t;
+		typedef uint8_t  u_int8_t;
 
 	#endif
-
 
 	/*
 	 * Custom stuff
@@ -300,9 +205,6 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 		#include <libkern/OSByteOrder.h>
 		#define __cpu_to_be64(x) = OSSwapHostToBigInt64(x)
 		#define __cpu_to_be32(x) = OSSwapHostToBigInt32(x)
-
-		#define cpu_to_be64(x) __cpu_to_be64(x)
-		#define cpu_to_be32(x) __cpu_to_be32(x)
 	#endif
 
 
@@ -324,18 +226,6 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 			#define __cpu_to_le32(x) ___my_swab32(x)
 			#define __cpu_to_le16(x) ___my_swab16(x)
 
-			#define be64_to_cpu(x) __be64_to_cpu(x)
-			#define be32_to_cpu(x) __be32_to_cpu(x)
-			#define be16_to_cpu(x) __be16_to_cpu(x)
-			#define cpu_to_be64(x) __cpu_to_be64(x)
-			#define cpu_to_be32(x) __cpu_to_be32(x)
-			#define cpu_to_be16(x) __cpu_to_be16(x)
-			#define le64_to_cpu(x) (x)
-			#define le32_to_cpu(x) (x)
-			#define le16_to_cpu(x) (x)
-			#define cpu_to_le64(x) (x)
-			#define cpu_to_le32(x) (x)
-			#define cpu_to_le16(x) (x)
 		#elif BYTE_ORDER == LITTLE_ENDIAN
 
 			#define __be64_to_cpu(x) ___my_swab64(x)
@@ -350,46 +240,112 @@ TODO: Cleanup and only have the BYTE_ORDER define set so it will define the macr
 			#define __cpu_to_le64(x) (x)
 			#define __cpu_to_le32(x) (x)
 			#define __cpu_to_le16(x) (x)
-
-			#define be64_to_cpu(x) __be64_to_cpu(x)
-			#define be32_to_cpu(x) __be32_to_cpu(x)
-			#define be16_to_cpu(x) __be16_to_cpu(x)
-			#define cpu_to_be64(x) __cpu_to_be64(x)
-			#define cpu_to_be32(x) __cpu_to_be32(x)
-			#define cpu_to_be16(x) __cpu_to_be16(x)
-			#define le64_to_cpu(x) (x)
-			#define le32_to_cpu(x) (x)
-			#define le16_to_cpu(x) (x)
-			#define cpu_to_le64(x) (x)
-			#define cpu_to_le32(x) (x)
-			#define cpu_to_le16(x) (x)
 		#endif
 	#endif
 
-	#ifndef MAX
-	#define MAX(x,y) ( (x)>(y) ? (x) : (y) )
+
+
+	// Common defines
+	#define cpu_to_le64 __cpu_to_le64
+	#define le64_to_cpu __le64_to_cpu
+	#define cpu_to_le32 __cpu_to_le32
+	#define le32_to_cpu __le32_to_cpu
+	#define cpu_to_le16 __cpu_to_le16
+	#define le16_to_cpu __le16_to_cpu
+	#define cpu_to_be64 __cpu_to_be64
+	#define be64_to_cpu __be64_to_cpu
+	#define cpu_to_be32 __cpu_to_be32
+	#define be32_to_cpu __be32_to_cpu
+	#define cpu_to_be16 __cpu_to_be16
+	#define be16_to_cpu __be16_to_cpu
+
+
+	#if (BYTE_ORDER == LITTLE_ENDIAN || __BYTE_ORDER == LITTLE_ENDIAN || __BYTE_ORDER == __LITTLE_ENDIAN)
+
+		#ifndef htobe16
+			#define htobe16 ___my_swab16
+		#endif
+		#ifndef htobe32
+			#define htobe32 ___my_swab32
+		#endif
+		#ifndef betoh16
+			#define betoh16 ___my_swab16
+		#endif
+		#ifndef betoh32
+			#define betoh32 ___my_swab32
+		#endif
+
+		#ifndef htole16
+			#define htole16(x) (x)
+		#endif
+		#ifndef htole32
+			#define htole32(x) (x)
+		#endif
+		#ifndef letoh16
+			#define letoh16(x) (x)
+		#endif
+		#ifndef letoh32
+			#define letoh32(x) (x)
+		#endif
+
 	#endif
 
-	#ifndef MIN
-	#define MIN(x,y) ( (x)>(y) ? (y) : (x) )
+	#if (BYTE_ORDER == BIG_ENDIAN || __BYTE_ORDER == BIG_ENDIAN || __BYTE_ORDER == __BIG_ENDIAN)
+
+		#ifndef htobe16
+			#define htobe16(x) (x)
+		#endif
+		#ifndef htobe32
+			#define htobe32(x) (x)
+		#endif
+		#ifndef betoh16
+			#define betoh16(x) (x)
+		#endif
+		#ifndef betoh32
+			#define betoh32(x) (x)
+		#endif
+
+		#ifndef htole16
+			#define htole16 ___my_swab16
+		#endif
+		#ifndef htole32
+			#define htole32 ___my_swab32
+		#endif
+		#ifndef letoh16
+			#define letoh16 ___my_swab16
+		#endif
+		#ifndef letoh32
+			#define letoh32 ___my_swab32
+		#endif
+
 	#endif
 
 
+	#ifndef le16toh
+		#define le16toh le16_to_cpu
+	#endif
+	#ifndef be16toh
+		#define be16toh be16_to_cpu
+	#endif
+	#ifndef le32toh
+		#define le32toh le32_to_cpu
+	#endif
+	#ifndef be32toh
+		#define be32toh be32_to_cpu
+	#endif
 
 
-	// Mac: Check http://www.opensource.apple.com/source/CF/CF-476.18/CFByteOrder.h
-	//      http://developer.apple.com/DOCUMENTATION/CoreFoundation/Reference/CFByteOrderUtils/Reference/reference.html
-	// Write to apple to ask what should be used.
-
-	// Only for aircrack-ng
-	#define CPUID_MMX_AVAILABLE 1
-	#define CPUID_SSE2_AVAILABLE 2
-	#define CPUID_NOTHING_AVAILABLE 0
-
-	#if defined(__i386__) || defined(__x86_64__)
-		#define CPUID() shasse2_cpuid()
-	#else
-		#define CPUID() CPUID_NOTHING_AVAILABLE
+	#ifndef htons
+		#define htons be16_to_cpu
+	#endif
+	#ifndef htonl
+		#define htonl cpu_to_be16
+	#endif
+	#ifndef ntohs
+		#define ntohs cpu_to_be16
+	#endif
+	#ifndef ntohl
+		#define ntohl cpu_to_be32
 	#endif
 
 #endif
