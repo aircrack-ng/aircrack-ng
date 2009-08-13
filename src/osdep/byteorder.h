913c39a45bc27aa1c3fb18a8842d602b7db11923
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2009-08-13 18:51:18 +0000

    Trying to (partially) fix compilation with gcc 2.95.
    git-svn-id: http://svn.aircrack-ng.org/trunk@1623 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/osdep/byteorder.h
+++ src/osdep/byteorder.h
@@ -51,10 +51,12 @@
 		#include <unistd.h>
 		#include <stdint.h>
 
-		typedef uint64_t u_int64_t;
-		typedef uint32_t u_int32_t;
-		typedef uint16_t u_int16_t;
-		typedef uint8_t  u_int8_t;
+		#ifndef __int8_t_defined
+			typedef uint64_t u_int64_t;
+			typedef uint32_t u_int32_t;
+			typedef uint16_t u_int16_t;
+			typedef uint8_t  u_int8_t;
+		#endif
 
 	#endif
 
