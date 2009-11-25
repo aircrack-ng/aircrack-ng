3bc5cf197fc2c513d1c58ea3f98678d45c349c85
Johannes Berg <johannes@sipsolutions.net>
2009-11-25 03:09:41 +0100

    code style cleanups

--- radiotap.c
+++ radiotap.c
@@ -111,10 +111,9 @@ static const struct ieee80211_radiotap_namespace radiotap_ns = {
  */
 
 int ieee80211_radiotap_iterator_init(
-    struct ieee80211_radiotap_iterator *iterator,
-    struct ieee80211_radiotap_header *radiotap_header,
-    int max_length,
-    const struct ieee80211_radiotap_vendor_namespaces *vns)
+	struct ieee80211_radiotap_iterator *iterator,
+	struct ieee80211_radiotap_header *radiotap_header,
+	int max_length, const struct ieee80211_radiotap_vendor_namespaces *vns)
 {
 	/* Linux only supports version 0 radiotap format */
 	if (radiotap_header->it_version)
@@ -215,7 +214,7 @@ static void find_ns(struct ieee80211_radiotap_iterator *iterator,
  */
 
 int ieee80211_radiotap_iterator_next(
-    struct ieee80211_radiotap_iterator *iterator)
+	struct ieee80211_radiotap_iterator *iterator)
 {
 	while (1) {
 		int hit = 0;
--- radiotap_iter.h
+++ radiotap_iter.h
@@ -66,12 +66,11 @@ struct ieee80211_radiotap_iterator {
 };
 
 extern int ieee80211_radiotap_iterator_init(
-   struct ieee80211_radiotap_iterator *iterator,
-   struct ieee80211_radiotap_header *radiotap_header,
-   int max_length,
-   const struct ieee80211_radiotap_vendor_namespaces *vns);
+	struct ieee80211_radiotap_iterator *iterator,
+	struct ieee80211_radiotap_header *radiotap_header,
+	int max_length, const struct ieee80211_radiotap_vendor_namespaces *vns);
 
 extern int ieee80211_radiotap_iterator_next(
-   struct ieee80211_radiotap_iterator *iterator);
+	struct ieee80211_radiotap_iterator *iterator);
 
 #endif /* __RADIOTAP_ITER_H */
