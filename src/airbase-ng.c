234d1eeef13c2f828273a2fe8e05775b671be68e
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2010-01-11 19:20:05 +0000

    Updated copyrights.
    git-svn-id: http://svn.aircrack-ng.org/trunk@1647 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airbase-ng.c
+++ src/airbase-ng.c
@@ -2,7 +2,7 @@
  *  802.11 monitor AP
  *  based on airtun-ng
  *
- *  Copyright (C) 2008, 2009 Thomas d'Otreppe
+ *  Copyright (C) 2008-2010 Thomas d'Otreppe
  *  Copyright (C) 2008, 2009 Martin Beck
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -149,7 +149,7 @@ extern const unsigned char crc_chop_tbl[256][4];
 
 char usage[] =
 "\n"
-"  %s - (C) 2008, 2009 Thomas d'Otreppe\n"
+"  %s - (C) 2008-2010 Thomas d'Otreppe\n"
 "  Original work: Martin Beck\n"
 "  http://www.aircrack-ng.org\n"
 "\n"
