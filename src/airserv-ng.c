1b8a0528c2a8ff3d53a02316ca83933a2bd6d785
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2011-04-30 03:03:26 +0000

    Airserv-ng: Fixed crash when client disconnect (Thanks gkruse).
    git-svn-id: http://svn.aircrack-ng.org/trunk@1893 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airserv-ng.c
+++ src/airserv-ng.c
@@ -453,6 +453,7 @@ static void serv(struct sstate *ss, char *dev, int port, int chan)
 	int max;
 	fd_set fds;
 	struct client *c;
+	struct client *next;	
 	int card_fd;
 
 	open_card_and_sock(ss, dev, port, chan);
@@ -485,10 +486,11 @@ static void serv(struct sstate *ss, char *dev, int port, int chan)
 		/* handle clients */
 		c = ss->ss_clients.c_next;
 		while (c != &ss->ss_clients) {
+			next = c->c_next;	
 			if (FD_ISSET(c->c_s, &fds))
 				handle_client(ss, c);
 
-			c = c->c_next;
+			c = next;
 		}
 
 		/* handle server */
