88c843c09e2d14c29a1bd4b02befbdfa4605ec44
David Francos <xayon@xayon.net>
2012-02-13 13:27:05 +0000

    Updated airgraph to do a proper installation with setup.py after receiving permission from thex1le
    git-svn-id: http://svn.aircrack-ng.org/trunk@2085 28c6078b-6c39-48e3-add9-af49d547ecab

---
+++ scripts/airgraph-ng/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# This file is Copyright David Francos Cuartero, licensed under the GPL2 license.
+
+from distutils.core import setup
+import os
+
+setup(name='airgraph-ng',
+      version='1.1',
+      description='Aircrack-ng network grapher',
+      author='TheX1le',
+      console = [{"script": "airgraph-ng" }],
+      url='http://aircrack-ng.org',
+      download_url='http://github.com/XayOn/socialstickers',
+      license='GPL2',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+      ],
+      packages=['graphviz'],
+      scripts=['dump-join', 'airgraph-ng'],
+     )
