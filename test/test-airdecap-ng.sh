cf42e8435702dfda01a243aea3d12c2235916ff3
Rick Farina <zero_chaos@aircrack-ng.org>
2013-02-21 04:18:20 +0000

    test-airdecap-ng.sh: adjust output and remove use of tee to avoid sandbox issues during build. no real need to touch /dev/stderr directly
    git-svn-id: http://svn.aircrack-ng.org/trunk@2248 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-airdecap-ng.sh
+++ test/test-airdecap-ng.sh
@@ -19,14 +19,14 @@ tmpdir="$(mktemp -d)"
 trap "rm -fr "${tmpdir}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM EXIT
 # Test1
 cp -f "${TESTDIR}/wpa.cap" "${tmpdir}"
-airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | tee /dev/stderr | grep -q "Number of decrypted WPA  packets         2"
+airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | grep "Number of decrypted WPA  packets         2"
 # Check that the hash is what we expect
 [[ $(sha1sum "${tmpdir}/wpa-dec.cap" | awk '{print $1}') == "69f8557cf96a26060989e88adfb521a01fc9b122" ]]
 # Test 2
 cp -f "${TESTDIR}/wpa-psk-linksys.cap" "${tmpdir}"
-airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | tee /dev/stderr | grep -q "Number of decrypted WPA  packets        53"
+airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | grep "Number of decrypted WPA  packets        53"
 [[ $(sha1sum "${tmpdir}/wpa-psk-linksys-dec.cap" | awk '{print $1}') == "1e75a9af0d9703c4ae4fc8ea454326aeb4abecc1" ]]
 # Test 3
 cp -f "${TESTDIR}/wpa2-psk-linksys.cap" "${tmpdir}"
-airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | tee /dev/stderr | grep -q "Number of decrypted WPA  packets        25"
+airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | grep "Number of decrypted WPA  packets        25"
 [[ $(sha1sum "${tmpdir}/wpa2-psk-linksys-dec.cap" | awk '{print $1}') == "2da107b96fbe19d926020ffb0da72553b18a5775" ]]
