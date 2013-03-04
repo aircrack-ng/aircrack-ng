83517459ae2effb862f3be5613974ba8a33065df
Carlos Alberto Lopez Perez <clopez@igalia.com>
2013-03-04 20:44:46 +0000

    ./test/test-airdecap-ng.sh: trivial fix
      * Keep consistency: Use curly braces for variables
    
    
    git-svn-id: http://svn.aircrack-ng.org/trunk@2256 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-airdecap-ng.sh
+++ test/test-airdecap-ng.sh
@@ -25,18 +25,18 @@ airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | \
 # For each hash there are two possibilities: little or big endian
 sha1sum=$(sha1sum "${tmpdir}/wpa-dec.cap" | awk '{print $1}')
 [[ "${sha1sum}" == "69f8557cf96a26060989e88adfb521a01fc9b122" ]] || \
-	[[ "$sha1sum" == "fb1592b2c0dccef542c1f46297394ee2892f8ed3" ]]
+	[[ "${sha1sum}" == "fb1592b2c0dccef542c1f46297394ee2892f8ed3" ]]
 # Test 2
 cp -f "${TESTDIR}/wpa-psk-linksys.cap" "${tmpdir}"
 airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | \
 	grep "Number of decrypted WPA  packets        53"
 sha1sum=$(sha1sum "${tmpdir}/wpa-psk-linksys-dec.cap" | awk '{print $1}')
 [[ "${sha1sum}" == "1e75a9af0d9703c4ae4fc8ea454326aeb4abecc1" ]] || \
-	[[ "$sha1sum"  == "1c3c4123ba6718bd3db66de251a125ed65cd6ee6" ]]
+	[[ "${sha1sum}"  == "1c3c4123ba6718bd3db66de251a125ed65cd6ee6" ]]
 # Test 3
 cp -f "${TESTDIR}/wpa2-psk-linksys.cap" "${tmpdir}"
 airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | \
 	grep "Number of decrypted WPA  packets        25"
 sha1sum=$(sha1sum "${tmpdir}/wpa2-psk-linksys-dec.cap" | awk '{print $1}')
 [[ "${sha1sum}" == "2da107b96fbe19d926020ffb0da72553b18a5775" ]] || \
-	[[ "$sha1sum" == "dc7d033b9759838d57b74db04185c3586cbd8042" ]]
+	[[ "${sha1sum}" == "dc7d033b9759838d57b74db04185c3586cbd8042" ]]
