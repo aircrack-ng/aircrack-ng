9d5f793c2a2f5182ff1c681ebf0b14a00dd1056d
Rick Farina <zero_chaos@aircrack-ng.org>
2013-10-05 01:08:31 +0000

    THIS PATCH WILL INTENTIONALLY FAIL TRAVIS.CI, THIS IS BECAUSE TESTS HAVE BEEN BROKEN FOREVER.  The scripts in the test directory were manipulating the path by adding the build src directory first then leaving the rest of the path, this appeared to work if you have aircrack-ng installed on the system because the make check target was only building aircrack-ng and _nothing_else_ so calls to airdecap-ng and airolib-ng would fall through to the system path and use the installed versions.  This patch FIXES the tests to actually run properly, however, since the tests haven't been running properly at all the airdecap-ng tests appear to be failing -Love, Zero
    git-svn-id: http://svn.aircrack-ng.org/trunk@2335 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-airdecap-ng.sh
+++ test/test-airdecap-ng.sh
@@ -8,18 +8,13 @@
 #
 set -e
 set -o pipefail
-# We receive from Makefile the path to src/ and we put it as first option on $PATH
-# This allows us to run the test against the recent compiled binary if we are called
-# from Makefile (make check). Otherwise we run the test against the installed binary
-# on the system.
-[[ -d "$1" ]] && export PATH="${1}:${PATH}"
 TESTDIR="$(dirname $0)"
 tmpdir="$(mktemp -d)"
 # Clean on exit
 trap "rm -fr "${tmpdir}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM EXIT
 # Test1
 cp -f "${TESTDIR}/wpa.cap" "${tmpdir}"
-airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | \
+./airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | \
 	grep "Number of decrypted WPA  packets         2"
 # Check that the hash is what we expect.
 # For each hash there are two possibilities: little or big endian
@@ -28,14 +23,14 @@ sha1sum=$(sha1sum "${tmpdir}/wpa-dec.cap" | awk '{print $1}')
 	[[ "${sha1sum}" == "fb1592b2c0dccef542c1f46297394ee2892f8ed3" ]]
 # Test 2
 cp -f "${TESTDIR}/wpa-psk-linksys.cap" "${tmpdir}"
-airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | \
+./airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | \
 	grep "Number of decrypted WPA  packets        53"
 sha1sum=$(sha1sum "${tmpdir}/wpa-psk-linksys-dec.cap" | awk '{print $1}')
 [[ "${sha1sum}" == "1e75a9af0d9703c4ae4fc8ea454326aeb4abecc1" ]] || \
 	[[ "${sha1sum}"  == "1c3c4123ba6718bd3db66de251a125ed65cd6ee6" ]]
 # Test 3
 cp -f "${TESTDIR}/wpa2-psk-linksys.cap" "${tmpdir}"
-airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | \
+./airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | \
 	grep "Number of decrypted WPA  packets        25"
 sha1sum=$(sha1sum "${tmpdir}/wpa2-psk-linksys-dec.cap" | awk '{print $1}')
 [[ "${sha1sum}" == "2da107b96fbe19d926020ffb0da72553b18a5775" ]] || \
--- test/test-airolib-sqlite.sh
+++ test/test-airolib-sqlite.sh
@@ -8,16 +8,11 @@
 #
 set -e
 set -o pipefail
-# We receive from Makefile the path to src/ and we put it as first option on $PATH
-# This allows us to run the test against the recent compiled binary if we are called
-# from Makefile (make check). Otherwise we run the test against the installed binary
-# on the system.
-[[ -d "$1" ]] && export PATH="${1}:${PATH}"
 TESTDIR="$(dirname $0)"
 tmpfile="$(mktemp -u)"
 # Clean on exit
 trap "rm -f "${tmpfile}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM EXIT
 echo Harkonen | airolib-ng "${tmpfile}" --import essid -
-airolib-ng "${tmpfile}" --import passwd "${TESTDIR}/password.lst"
-airolib-ng "${tmpfile}" --batch | grep "Computed 233 PMK"
-aircrack-ng -q -e Harkonen  -r "${tmpfile}"  "${TESTDIR}/wpa2.eapol.cap" | grep 'KEY FOUND! \[ 12345678 \]'
+./airolib-ng "${tmpfile}" --import passwd "${TESTDIR}/password.lst"
+./airolib-ng "${tmpfile}" --batch | grep "Computed 233 PMK"
+./aircrack-ng -q -e Harkonen  -r "${tmpfile}"  "${TESTDIR}/wpa2.eapol.cap" | grep 'KEY FOUND! \[ 12345678 \]'
