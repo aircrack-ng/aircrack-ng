276b432f7147c524883443608ccab13f58c88432
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2014-10-31 20:43:14 +0000

    Prepare for release: RC1.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2424 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/version.h
+++ src/version.h
@@ -33,8 +33,8 @@
 #define _MAJ 1
 #define _MIN 2
 #define _SUB_MIN 0
-#define _BETA 3
-#define _RC 0
+#define _BETA 0
+#define _RC 1
 #define WEBSITE "http://www.aircrack-ng.org"
 
 extern char * getVersion(char * progname, int maj, int min, int submin, int
