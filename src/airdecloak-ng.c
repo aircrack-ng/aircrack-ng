105b86eb3da6748c6d636effdd364fea2676cd09
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-02-01 04:15:22 +0000

    Airdecloak-ng: Merge partial pull request #22 from https://github.com/aircrack-ng/aircrack-ng/pull/22 - Patch by Kelvin You (weyou):  Fixed compilation error on -DDEBUG mode
    git-svn-id: http://svn.aircrack-ng.org/trunk@2465 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airdecloak-ng.c
+++ src/airdecloak-ng.c
@@ -547,7 +547,7 @@ BOOLEAN read_packets(void)
         }
 
 		#ifdef DEBUG
-        printf("From DS: %d - ToDS: %d\n", _packet_elt_head->current->fromDS, packet->toDS);
+        printf("From DS: %d - ToDS: %d\n", _packet_elt_head->current->fromDS, _packet_elt_head->current->toDS);
         printf("BSSID: %02X:%02X:%02X:%02X:%02X:%02X\n", _packet_elt_head->current->bssid[0],
 				_packet_elt_head->current->bssid[1], _packet_elt_head->current->bssid[2],
 				_packet_elt_head->current->bssid[3], _packet_elt_head->current->bssid[4],
@@ -614,7 +614,7 @@ BOOLEAN read_packets(void)
 			// Copy key index
 			_packet_elt_head->current->key_index = h80211[27];
 			#ifdef DEBUG
-			printf("Key index: %d\n", packet->key_index);
+			printf("Key index: %d\n", _packet_elt_head->current->key_index);
 			#endif
 			// Copy checksum
 			memcpy(_packet_elt_head->current->icv, buffer + (_packet_elt_head->current->header.caplen) - 4, 4);
