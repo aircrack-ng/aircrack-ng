1a34f3087b1d64910162cf2b1c9ca24005964095
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-02-22 23:41:06 +0000

    Airodump-ng: For NetXML, connected clients will be 'established' (there are other values but that one is acceptable) and 'tods' for unassociated clients.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2470 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/airodump-ng.c
+++ src/airodump-ng.c
@@ -3894,9 +3894,10 @@ int dump_write_kismet_netxml_client_info(struct ST_info *client, int client_no)
 	last_time[strlen(last_time) - 1] = 0; // remove new line
 
 	fprintf(G.f_kis_xml, "\t\t<wireless-client number=\"%d\" "
-				 "type=\"established\" first-time=\"%s\""
+				 "type=\"%s\" first-time=\"%s\""
 				 " last-time=\"%s\">\n",
-				 client_no, first_time, last_time );
+				 client_no, (st_cur->base == NULL) ? "tods" : "established",
+				 first_time, last_time );
 
 	fprintf( G.f_kis_xml, "\t\t\t<client-mac>%02X:%02X:%02X:%02X:%02X:%02X</client-mac>\n",
 				 client->stmac[0], client->stmac[1],
