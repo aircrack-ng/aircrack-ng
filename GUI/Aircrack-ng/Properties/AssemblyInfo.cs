8f6e2503147859451e18fc35eef81cdd0b7a3bb2
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-11-20 04:22:26 +0000

    Aircrack-ng GUI: Updated version in assembly.
    git-svn-id: http://svn.aircrack-ng.org/branch/Windows@2729 28c6078b-6c39-48e3-add9-af49d547ecab

--- GUI/Aircrack-ng/Properties/AssemblyInfo.cs
+++ GUI/Aircrack-ng/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
