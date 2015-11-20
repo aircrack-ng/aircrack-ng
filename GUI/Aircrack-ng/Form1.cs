efeb1febb75818c326f1e965fe0a06ecd4b28b78
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-11-20 04:21:02 +0000

    Aircrack-ng GUI: Cleanup includes.
    git-svn-id: http://svn.aircrack-ng.org/branch/Windows@2728 28c6078b-6c39-48e3-add9-af49d547ecab

--- GUI/Aircrack-ng/Form1.cs
+++ GUI/Aircrack-ng/Form1.cs
@@ -19,11 +19,6 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
