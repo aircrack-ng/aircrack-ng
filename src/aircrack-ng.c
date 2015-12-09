2eda7aef01780d8d8a38e70616a57aeed3659b10
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-12-09 02:58:41 +0000

    Fixed compilation with clang.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2767 28c6078b-6c39-48e3-add9-af49d547ecab

--- src/aircrack-ng.c
+++ src/aircrack-ng.c
@@ -433,6 +433,9 @@ void eof_wait( int *eof_notified )
 	usleep( 100000 );
 }
 
+#ifdef __clang__
+int wpa_send_passphrase(char *key, struct WPA_data* data, int lock);
+#endif
 
 inline int wpa_send_passphrase(char *key, struct WPA_data* data, int lock)
 {
@@ -495,6 +498,9 @@ inline int wpa_send_passphrase(char *key, struct WPA_data* data, int lock)
 	return 1;
 }
 
+#ifdef __clang__
+int wpa_receive_passphrase(char *key, struct WPA_data* data);
+#endif
 
 inline int wpa_receive_passphrase(char *key, struct WPA_data* data)
 {
