083f5facc86f97a82e16c115b006b068b86f265f
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-12-09 06:11:12 +0000

    Converted hex string test script to sh.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2772 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-hex_string_to_array.sh
+++ test/test-hex_string_to_array.sh
@@ -1,7 +1,4 @@
-#! /bin/bash
-
-HEX_FAILING=('F' 'A' 'a' '1' '9' 'G' 'AG' '9U' 'aO' 'FF:FF:FF:AS' 'BLAH')
-HEX_SUCCESS=('FF' 'AA' 'aa' '11' '22' 'FF:AA:FF' 'C0:FF:EE:' '00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD:EE:FF')
+#! /bin/sh
 
 ./test-hex_string_to_array >/dev/null 2>/dev/null
 RET=$?
@@ -11,27 +8,48 @@ if [ ${RET} -ne 1 ]; then
 fi
 echo "Test Hex string to array with empty string: failure - Test successful"
 
-for failure in ${HEX_FAILING[@]}
-do
-	./test-hex_string_to_array "${failure}" >/dev/null 2>/dev/null
+hex_failing_test() {
+	./test-hex_string_to_array "${1}" >/dev/null 2>/dev/null
 	RET=$?
 	if [ ${RET} -ne 2 ]; then
-		echo "Hex string failed. Expected return value of 2 (failure), got ${RET} with ${failure}."
+	echo "Hex string failed. Expected return value of 2 (failure), got ${RET} with ${failure}."
 		exit 1
 	fi
 	echo "Test Hex string to array with ${failure}: failure - Test successful"
-done
+}
 
-for success in ${HEX_SUCCESS[@]}
-do
-	./test-hex_string_to_array "${success}" >/dev/null 2>/dev/null
+# sh does not support arrays, so we have to do it this way
+hex_failing_test 'F'
+hex_failing_test 'A'
+hex_failing_test 'a'
+hex_failing_test '1'
+hex_failing_test '9'
+hex_failing_test 'G'
+hex_failing_test 'AG'
+hex_failing_test '9U'
+hex_failing_test 'aO'
+hex_failing_test 'FF:FF:FF:AS'
+hex_failing_test 'BLAH'
+
+
+hex_success_test() {
+	./test-hex_string_to_array "${1}" >/dev/null 2>/dev/null
 	RET=$?
 	if [ ${RET} -ne 0 ]; then
 		echo "Hex string test failed. Expected return value of 0 (success), got ${RET} with ${success}"
 		exit 1
 	fi
 	echo "Test Hex string to array with ${success}: success - Test successful"
-done
+}
+
+hex_success_test 'FF'
+hex_success_test 'AA'
+hex_success_test 'aa'
+hex_success_test '11'
+hex_success_test '22'
+hex_success_test 'FF:AA:FF'
+hex_success_test 'C0:FF:EE:'
+hex_success_test '00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD:EE:FF'
 
 echo "Hex string tests successful."
 exit 0
