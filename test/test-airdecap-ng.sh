aa37be70bbad2a3371c6049c154f31818c0d9743
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-12-09 06:01:06 +0000

    Fixed airdecap-ng test shell script (make check) to work on FreeBSD with 'sh'.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2771 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-airdecap-ng.sh
+++ test/test-airdecap-ng.sh
@@ -1,13 +1,7 @@
-#! /bin/bash
+#! /bin/sh
 #
 # Carlos Alberto Lopez Perez <clopez@igalia.com>
-#
-# This will fail ("set -e" + "set -o pipefail") if any error happens.
-# So:
-#    If this exits with zero the test is correct. Otherwise the test failed
-#
-set -e
-set -o pipefail
+# Thomas d'Otreppe <tdotreppe@aircrack-ng.org> - Support for sha1 and sh
 TESTDIR="$(dirname $0)"
 tmpdir="$(mktemp -d)"
 # Clean on exit
@@ -15,23 +9,50 @@ trap "rm -fr "${tmpdir}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM
 # Test1
 cp -f "${TESTDIR}/wpa.cap" "${tmpdir}"
 ./airdecap-ng -e test -p biscotte "${tmpdir}/wpa.cap" | \
-	grep "Number of decrypted WPA  packets         2"
+        grep "Number of decrypted WPA  packets         2" || exit 1
+[ $? -ne 0 ] && exit 1
 # Check that the hash is what we expect.
 # For each hash there are two possibilities: little or big endian
-sha1sum=$(sha1sum "${tmpdir}/wpa-dec.cap" | awk '{print $1}')
-[[ "${sha1sum}" == "69f8557cf96a26060989e88adfb521a01fc9b122" ]] || \
-	[[ "${sha1sum}" == "fb1592b2c0dccef542c1f46297394ee2892f8ed3" ]]
+if ! type "sha1sum" > /dev/null 2>/dev/null ; then
+        sha1sum=$(sha1 -q "${tmpdir}/wpa-dec.cap")
+else
+        sha1sum=$(sha1sum "${tmpdir}/wpa-dec.cap" | awk '{print $1}')
+fi
+
+if [ "${sha1sum}" != "69f8557cf96a26060989e88adfb521a01fc9b122" ] &&
+        [ "${sha1sum}" != "fb1592b2c0dccef542c1f46297394ee2892f8ed3" ]; then
+        exit 1
+fi
+
 # Test 2
 cp -f "${TESTDIR}/wpa-psk-linksys.cap" "${tmpdir}"
 ./airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa-psk-linksys.cap" | \
-	grep "Number of decrypted WPA  packets        53"
-sha1sum=$(sha1sum "${tmpdir}/wpa-psk-linksys-dec.cap" | awk '{print $1}')
-[[ "${sha1sum}" == "1e75a9af0d9703c4ae4fc8ea454326aeb4abecc1" ]] || \
-	[[ "${sha1sum}"  == "1c3c4123ba6718bd3db66de251a125ed65cd6ee6" ]]
+        grep "Number of decrypted WPA  packets        53"
+[ $? -ne 0 ] && exit 1
+if ! type "sha1sum" > /dev/null 2>/dev/null ; then
+        sha1sum=$(sha1 -q "${tmpdir}/wpa-psk-linksys-dec.cap")
+else
+        sha1sum=$(sha1sum "${tmpdir}/wpa-psk-linksys-dec.cap" | awk '{print $1}')
+fi
+
+if [ "${sha1sum}" != "1e75a9af0d9703c4ae4fc8ea454326aeb4abecc1" ] &&
+        [ "${sha1sum}"  != "1c3c4123ba6718bd3db66de251a125ed65cd6ee6" ]; then
+        exit 1
+fi
+
 # Test 3
 cp -f "${TESTDIR}/wpa2-psk-linksys.cap" "${tmpdir}"
 ./airdecap-ng -e linksys -p dictionary "${tmpdir}/wpa2-psk-linksys.cap" | \
-	grep "Number of decrypted WPA  packets        25"
-sha1sum=$(sha1sum "${tmpdir}/wpa2-psk-linksys-dec.cap" | awk '{print $1}')
-[[ "${sha1sum}" == "2da107b96fbe19d926020ffb0da72553b18a5775" ]] || \
-	[[ "${sha1sum}" == "dc7d033b9759838d57b74db04185c3586cbd8042" ]]
+        grep "Number of decrypted WPA  packets        25"
+[ $? -ne 0 ] && exit 1
+if ! type "sha1sum" > /dev/null 2>/dev/null ; then
+        sha1sum=$(sha1 -q "${tmpdir}/wpa2-psk-linksys-dec.cap")
+else
+        sha1sum=$(sha1sum "${tmpdir}/wpa2-psk-linksys-dec.cap" | awk '{print $1}')
+fi
+
+if [ "${sha1sum}" != "2da107b96fbe19d926020ffb0da72553b18a5775" ] &&
+        [ "${sha1sum}" != "dc7d033b9759838d57b74db04185c3586cbd8042" ]; then
+        exit 1
+fi
+exit 0
