2ebfe2509d929498a5d111ca65d85442f30c1819
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2015-12-20 23:42:38 +0000

    Testing: Fixed airolib-ng test on FreeBSD.
    git-svn-id: http://svn.aircrack-ng.org/trunk@2797 28c6078b-6c39-48e3-add9-af49d547ecab

--- test/test-airolib-sqlite.sh
+++ test/test-airolib-sqlite.sh
@@ -1,18 +1,22 @@
-#! /bin/bash
+#! /bin/sh
 #
 # Carlos Alberto Lopez Perez <clopez@igalia.com>
 #
-# This will fail ("set -e" + "set -o pipefail") if any error happens.
-# So:
-#    If this exits with zero the test is correct. Otherwise the test failed
-#
-set -e
-set -o pipefail
 TESTDIR="$(dirname $0)"
-tmpfile="$(mktemp -d -t acng.XXXX)"
+tmpfile="$(mktemp -u -t acng.XXXX)"
 # Clean on exit
-trap "rm -f "${tmpfile}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM EXIT
+trap "rm -fr "${tmpdir}"" SIGINT SIGKILL SIGQUIT SIGSEGV SIGPIPE SIGALRM SIGTERM EXIT
+
 echo Harkonen | ./airolib-ng "${tmpfile}" --import essid -
+[ $? -ne 0 ] && exit 1
+
 ./airolib-ng "${tmpfile}" --import passwd "${TESTDIR}/password.lst"
+[ $? -ne 0 ] && exit 1
+
 ./airolib-ng "${tmpfile}" --batch | grep "Computed 233 PMK"
+[ $? -ne 0 ] && exit 1
+
 ./aircrack-ng -q -e Harkonen  -r "${tmpfile}"  "${TESTDIR}/wpa2.eapol.cap" | grep 'KEY FOUND! \[ 12345678 \]'
+[ $? -ne 0 ] && exit 1
+
+exit 0
