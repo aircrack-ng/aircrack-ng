9fafde1c85e2705488b7fffdf6275b3889ec513a
Johannes Berg <johannes@sipsolutions.net>
2016-10-25 12:32:05 +0200

    add VHT/timestamp fields to parser list
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- radiotap.c
+++ radiotap.c
@@ -39,6 +39,8 @@ static const struct radiotap_align_size rtap_namespace_sizes[] = {
 	[IEEE80211_RADIOTAP_DATA_RETRIES] = { .align = 1, .size = 1, },
 	[IEEE80211_RADIOTAP_MCS] = { .align = 1, .size = 3, },
 	[IEEE80211_RADIOTAP_AMPDU_STATUS] = { .align = 4, .size = 8, },
+	[IEEE80211_RADIOTAP_VHT] = { .align = 2, .size = 12, },
+	[IEEE80211_RADIOTAP_TIMESTAMP] = { .align = 8, .size = 12, },
 	/*
 	 * add more here as they are defined in radiotap.h
 	 */
