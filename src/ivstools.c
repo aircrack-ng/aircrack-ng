eddabaad9c65335096aab514038dff9d1def8751
Mister X <3520734+Mister-X-@users.noreply.github.com>
2018-06-07 17:03:52 +0000

    IVStools: Fixed prototypes.

--- src/ivstools.c
+++ src/ivstools.c
@@ -100,7 +100,7 @@ static unsigned char ZERO[32] =
         "\x00\x00\x00\x00\x00\x00\x00\x00"
         "\x00\x00\x00\x00\x00\x00\x00\x00";
 
-void usage(int what)
+static void usage(int what)
 {
     char *version_info = getVersion("ivsTools", _MAJ, _MIN, _SUB_MIN, _REVISION, _BETA, _RC);
     printf("\n  %s - (C) 2006-2018 Thomas d\'Otreppe\n"
@@ -117,7 +117,7 @@ void usage(int what)
                 "        Merge ivs files\n");
 }
 
-int merge( int argc, char *argv[] )
+static int merge( int argc, char *argv[] )
 {
     int i, n, unused;
     unsigned long nbw;
@@ -223,7 +223,7 @@ int merge( int argc, char *argv[] )
     return( 0 );
 }
 
-int dump_add_packet( unsigned char *h80211, unsigned caplen)
+static int dump_add_packet( unsigned char *h80211, unsigned caplen)
 {
     int i, n, seq, dlen, clen;
     unsigned z;
