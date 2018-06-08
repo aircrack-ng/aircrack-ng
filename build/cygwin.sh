9baeacdf04caff01bf56e61c7f1a47ca3ab4aaf9
Joseph Benden <joe@benden.us>
2018-06-08 12:01:24 -0700

    An attempt to resolve AppVeyor intermittent failures.

---
+++ build/cygwin.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euf
+
+COMPILER=gcc
+
+if [ $# -gt 0 ]
+then
+	COMPILER="${1}"
+	shift
+fi
+
+echo "I: ${COMPILER} compiler"
+
+if [ "x${COMPILER}" = xclang ]
+then
+	CC=clang-5.0
+	CXX=clang++-5.0
+	LIBS='-liconv'
+
+	export CC CXX LIBS
+fi
+
+RETRY=0
+
+while [ $RETRY -lt 3 ];
+do
+	./autogen.sh "$*" && break
+
+	echo "W: failed to run autogen.sh, will retry..."
+	RETRY=$(($RETRY + 1))
+done
+
+if [ $RETRY -ge 3 ];
+then
+	echo "F: All retries failed, aborting..."
+	exit 1
+fi
+
+make
+make check || { cat test/test-suite.log && exit 1; }
+make clean
+
+exit 0
