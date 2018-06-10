a5cba9d4199f375bd6a040f259d0afcf7adeb926
Joseph Benden <joe@benden.us>
2018-06-10 13:19:47 -0700

    Travis: Publish code coverage metrics.

---
+++ build/code_coverage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eufx
+
+#
+# Bail on OS X for testing this functionality.
+#
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    exit 0
+fi
+
+find . -name .deps -o -name '*.la' -o -name .libs -o -name Makefile -print0 | xargs -0 rm -vfr
+
+autoreconf -vi
+env CFLAGS="-Og -g3 -Werror" CXXFLAGS="-Og -g3 -Werror" \
+    ./configure --enable-shared --with-experimental --enable-code-coverage
+make
+make check
+
+lcov --compat-libtool --directory . --capture --rc lcov_branch_coverage=1 --output-file coverage.info
+
+lcov --remove coverage.info '/tmp/*' '/usr/include/*' '/usr/local/include/*' '/usr/lib/gcc/*' '/usr/local/lib/gcc/*' --rc lcov_branch_coverage=1 -o `pwd`/coverage_filtered.info
+
+cpp-coveralls -n -l coverage_filtered.info
+
+exit 0
