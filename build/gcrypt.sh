3fa6fe36a601483df68dc71aa54174f1e4f74e6a
Joseph Benden <joe@benden.us>
2018-06-30 14:01:45 -0700

    Travis: Ensure config.log is output upon failures.

--- build/gcrypt.sh
+++ build/gcrypt.sh
@@ -7,7 +7,7 @@ case "${CC:=}" in
     clang*|llvm*) EXTRA="--enable-asan";;
 esac
 
-./autogen.sh --with-experimental --with-gcrypt ${EXTRA}
+./autogen.sh --with-experimental --with-gcrypt ${EXTRA} || { cat config.log; exit 1; }
 make
 make check || { find test -name 'test-suite.log' -exec cat {} ';' && exit 1; }
 make clean
--- build/openssl.sh
+++ build/openssl.sh
@@ -7,7 +7,7 @@ case "${CC:=}" in
     clang*|llvm*) EXTRA="--enable-asan";;
 esac
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then ./autogen.sh --with-experimental --with-openssl=/usr/local/Cellar/openssl/1.0.2l ${EXTRA}; else ./autogen.sh --with-experimental ${EXTRA}; fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then ./autogen.sh --with-experimental --with-openssl=/usr/local/Cellar/openssl/1.0.2l ${EXTRA}; else ./autogen.sh --with-experimental ${EXTRA}; fi || { cat config.log; exit 1; }
 make
 make check || { find test -name 'test-suite.log' -exec cat {} ';' && exit 1; }
 make clean
