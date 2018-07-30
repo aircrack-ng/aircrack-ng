b1ca15666c208bc6c675a22a0bc70e37e6f4df1a
Joseph Benden <joe@benden.us>
2018-07-30 09:38:44 -0700

    Adds initial implementation of 802.11w cracking.

--- src/aircrack-crypto/crypto_engine.c
+++ src/aircrack-crypto/crypto_engine.c
@@ -338,6 +338,132 @@ EXPORT void ac_crypto_engine_calc_ptk(ac_crypto_engine_t *engine,
 	}
 }
 
+
+#define HAVE_OPENSSL_CMAC_H 1
+#include <openssl/cmac.h>
+
+#if HAVE_OPENSSL_CMAC_H
+
+/* Code borrowed from https://w1.fi/wpa_supplicant/ starts */
+
+#define SHA256_MAC_LEN 32
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+static inline void WPA_PUT_LE16(u8 *a, u16 val)
+{
+	a[1] = (u8) (val >> 8u);
+	a[0] = (u8) (val & 0xff);
+}
+
+static void sha256_vector(size_t num_elem, const u8 *addr[], const size_t *len, u8 *mac)
+{
+	SHA256_CTX ctx;
+	size_t i;
+
+	SHA256_Init(&ctx);
+	for (i = 0; i < num_elem; i++) {
+		SHA256_Update(&ctx, addr[i], len[i]);
+	}
+
+	SHA256_Final(mac, &ctx);
+}
+
+static void hmac_sha256_vector(const u8 *key, size_t key_len, size_t num_elem,
+		const u8 *addr[], const size_t *len, u8 *mac)
+{
+	unsigned char k_pad[64]; /* padding - key XORd with ipad/opad */
+	const u8 *_addr[6];
+	size_t _len[6], i;
+
+	/* the HMAC_SHA256 transform looks like:
+	 *
+	 * SHA256(K XOR opad, SHA256(K XOR ipad, text))
+	 *
+	 * where K is an n byte key
+	 * ipad is the byte 0x36 repeated 64 times
+	 * opad is the byte 0x5c repeated 64 times
+	 * and text is the data being protected */
+
+	/* start out by storing key in ipad */
+	memset(k_pad, 0, sizeof(k_pad));
+	memcpy(k_pad, key, key_len);
+	/* XOR key with ipad values */
+	for (i = 0; i < 64; i++)
+		k_pad[i] ^= 0x36;
+
+	/* perform inner SHA256 */
+	_addr[0] = k_pad;
+	_len[0] = 64;
+	for (i = 0; i < num_elem; i++) {
+		_addr[i + 1] = addr[i];
+		_len[i + 1] = len[i];
+	}
+	sha256_vector(1 + num_elem, _addr, _len, mac);
+
+	memset(k_pad, 0, sizeof(k_pad));
+	memcpy(k_pad, key, key_len);
+	/* XOR key with opad values */
+	for (i = 0; i < 64; i++)
+		k_pad[i] ^= 0x5c;
+
+	/* perform outer SHA256 */
+	_addr[0] = k_pad;
+	_len[0] = 64;
+	_addr[1] = mac;
+	_len[1] = SHA256_MAC_LEN;
+	sha256_vector(2, _addr, _len, mac);
+}
+
+static void sha256_prf_bits(const u8 *key, size_t key_len, const char *label,
+		const u8 *data, size_t data_len, u8 *buf, size_t buf_len_bits)
+{
+	u16 counter = 1;
+	size_t pos, plen;
+	u8 hash[SHA256_MAC_LEN];
+	const u8 *addr[4];
+	size_t len[4];
+	u8 counter_le[2], length_le[2];
+	size_t buf_len = (buf_len_bits + 7) / 8;
+
+	addr[0] = counter_le;
+	len[0] = 2;
+	addr[1] = (u8 *) label;
+	len[1] = strlen(label);
+	addr[2] = data;
+	len[2] = data_len;
+	addr[3] = length_le;
+	len[3] = sizeof(length_le);
+
+	WPA_PUT_LE16(length_le, (u16) buf_len_bits);
+	pos = 0;
+
+	while (pos < buf_len) {
+		plen = buf_len - pos;
+		WPA_PUT_LE16(counter_le, counter);
+		if (plen >= SHA256_MAC_LEN) {
+			hmac_sha256_vector(key, key_len, 4, addr, len, &buf[pos]);
+			pos += SHA256_MAC_LEN;
+		} else {
+			hmac_sha256_vector(key, key_len, 4, addr, len, hash);
+			memcpy(&buf[pos], hash, plen);
+			pos += plen;
+			break;
+		}
+		counter++;
+	}
+
+	/*
+	 * Mask out unused bits in the last octet if it does not use all the
+	 * bits.
+	 */
+	if (buf_len_bits % 8) {
+		u8 mask = (u8) (0xff << (8u - buf_len_bits % 8));
+		buf[pos - 1] &= mask;
+	}
+}
+#endif /* HAVE_OPENSSL_CMAC_H */
+
 EXPORT void ac_crypto_engine_calc_mic(ac_crypto_engine_t *engine,
 									  const uint8_t eapol[256],
 									  const uint32_t eapol_size,
@@ -356,7 +482,7 @@ EXPORT void ac_crypto_engine_calc_mic(ac_crypto_engine_t *engine,
 			 eapol_size,
 			 mic[vectorIdx],
 			 NULL);
-	else
+	else if (keyver == 2)
 		HMAC(EVP_sha1(),
 			 &ptk[vectorIdx],
 			 16,
@@ -364,6 +490,36 @@ EXPORT void ac_crypto_engine_calc_mic(ac_crypto_engine_t *engine,
 			 eapol_size,
 			 mic[vectorIdx],
 			 NULL);
+#if HAVE_OPENSSL_CMAC_H
+	else if (keyver == 3)
+	{
+		uint8_t data[64 + 12];
+		size_t miclen;
+		CMAC_CTX *ctx;
+
+		wpapsk_hash *pmk = engine->thread_data[threadid]->pmk;
+		uint8_t *pke = &engine->thread_data[threadid]->pke[23];
+
+		memset(data, 0, sizeof(data));
+		memcpy(data, pke, 6);
+		memcpy(data + 6, pke + 6, 6);
+		memcpy(data + 12, pke + 35 - 23, 64);
+
+		sha256_prf_bits((unsigned char*)(pmk[vectorIdx].v), 32, "Pairwise key expansion", data, 76, ptk, 48 * 8); // PTK
+
+		// Compute MIC
+		ctx = CMAC_CTX_new();
+		CMAC_Init(ctx, ptk, 16, EVP_aes_128_cbc(), 0);
+		CMAC_Update(ctx, eapol, eapol_size);
+		CMAC_Final(ctx, mic[vectorIdx], &miclen);
+		CMAC_CTX_free(ctx);
+	}
+#endif /* HAVE_OPENSSL_CMAC_H */
+	else
+	{
+		fprintf(stderr, "Unsupported key version %d encountered.\n", keyver);
+		abort();
+	}
 }
 
 EXPORT int
@@ -383,7 +539,8 @@ ac_crypto_engine_wpa_crack(ac_crypto_engine_t *engine,
 	{
 		/* compute the pairwise transient key and the frame MIC */
 
-		ac_crypto_engine_calc_ptk(engine, j, threadid);
+		if (keyver < 3)
+			ac_crypto_engine_calc_ptk(engine, j, threadid);
 
 		ac_crypto_engine_calc_mic(
 			engine, eapol, eapol_size, mic, keyver, j, threadid);
