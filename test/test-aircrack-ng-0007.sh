6a8b10a422636e37b32110f1cf8c002fb02a1942
Joseph Benden <joe@benden.us>
2018-07-30 10:36:15 -0700

    Added 802.11w unit-test.
    While this successfully tests 802.11w cracking, it should be noted
    that password-2.lst was created to PREVENT airolib-ng from
    failing to pass its' own unit-testing. It is a bug.

---
+++ test/test-aircrack-ng-0007.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ef
+
+"${top_builddir}/src/aircrack-ng${EXEEXT}" \
+    ${AIRCRACK_NG_ARGS} \
+    -w "${abs_srcdir}/password-2.lst" \
+    -a 3 \
+    -e Neheb \
+    -q "${abs_srcdir}/n-02.cap" | \
+        grep 'KEY FOUND! \[ bo$$password \]'
+
+exit 0
+
