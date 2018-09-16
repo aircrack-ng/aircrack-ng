2c70a50433e7f5d5029f1c1d1a63eacc9638aa27
Chris Lundquist <rampantdurandal@gmail.com>
2018-09-16 10:58:50 -0700

    fix test 10 to use the right key and run with one thread for now to work around the bogus answer

--- test/test-aircrack-ng-0010.sh
+++ test/test-aircrack-ng-0010.sh
@@ -5,10 +5,10 @@ set -ef
 
 "${top_builddir}/src/aircrack-ng${EXEEXT}" \
     ${AIRCRACK_NG_ARGS} \
-    -K \
+    -X -K \
     "${abs_srcdir}/test.ivs" \
     -l /dev/null | \
-        grep "KEY FOUND" | grep "AE:5B:7F:3A:03:D0:AF:9B:F6:8D:A5:BB:C7"
+        grep "KEY FOUND" | grep "AE:5B:7F:3A:03:D0:AF:9B:F6:8D:A5:E2:C7"
 
 exit 0
 
