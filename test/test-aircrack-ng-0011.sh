60b8592cfc398849f269a85ee2d002f2307bba1e
Joseph Benden <joe@benden.us>
2018-09-17 12:22:52 -0700

    Added additional unit-tests to exercise WEP cracking.

---
+++ test/test-aircrack-ng-0011.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ef
+
+"${top_builddir}/src/aircrack-ng${EXEEXT}" \
+    ${AIRCRACK_NG_ARGS} \
+    -b 00:11:95:91:78:8C \
+    -K \
+    "${abs_srcdir}/test.ivs" \
+    -l /dev/null | \
+        grep "KEY FOUND" | grep "AE:5B:7F:3A:03:D0:AF:9B:F6:8D:A5:E2:C7"
+
+exit 0
+
