14adce9ef296f4cdeaa7cc35258c707bf3f03d61
Chris Lundquist <rampantdurandal@gmail.com>
2018-09-24 17:51:52 -0700

    rename

--- test/test-aircrack-ng-0015.sh
+++ test/test-aircrack-ng-0015.sh
@@ -2,13 +2,11 @@
 
 set -ef
 
-# if test ! -z "${CI}"; then exit 77; fi
-#echo "staytogether" | ./src/aircrack-ng -e "Stay Alfred" ./test/test.hccpx -w -
 echo "staytogether" | "${top_builddir}/src/aircrack-ng${EXEEXT}" \
     ${AIRCRACK_NG_ARGS} \
     -e "Stay Alfred" \
     -w - \
-    "${abs_srcdir}/test.hccpx" \
+    "${abs_srcdir}/StayAlfred.hccapx" \
     -l /dev/null | \
         grep "KEY FOUND"
 
