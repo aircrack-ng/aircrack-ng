792397cfa52369176e2c1f82f405595eb4dff2b3
Joseph Benden <joe@benden.us>
2018-09-25 15:13:02 -0700

    Travis: Only check on Linux and ensure tools are installed.

--- build/travis-formatting.sh
+++ build/travis-formatting.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#
+# Bail on OS X for testing this functionality.
+#
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    exit 0
+fi
+
+#
+# Only works with GCC.
+#
+case "$CC" in
+    clang*|llvm*) exit 0;;
+esac
+
+sudo add-apt-repository -y 'deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.8 main'
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get install --allow-unauthenticated -y -qq clang-format-3.8
+
 # check formatting matches clang-format-3.8. Since newer versions can have
 # changes in formatting even without any rule changes, we have to fix on a
 # single version.
