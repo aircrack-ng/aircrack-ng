af442062a7ffdc4d94aedf02ebcefd59831137dc
Joseph Benden <joe@benden.us>
2019-01-11 10:42:02 -0700

    Added missing headers.

--- src/packetforge-ng.c
+++ src/packetforge-ng.c
@@ -35,9 +35,11 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <time.h>
 #include <limits.h>
 #include <getopt.h>
