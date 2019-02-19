03d8e4376572d50c560dc54c9c0758be0e84e748
Mister X <3520734+Mister-X-@users.noreply.github.com>
2019-02-19 17:53:45 +0000

    Tests: Check for tcpdump in injection tests

--- test/test-aireplay-ng-0002.sh
+++ test/test-aireplay-ng-0002.sh
@@ -20,6 +20,12 @@ if [ $? -ne 0 ]; then
 	exit 77
 fi
 
+hash tcpdump 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+	echo "tcpdump is not installed, skipping"
+	exit 77
+fi
+
 # Load module
 LOAD_MODULE=0
 if [ $(lsmod | egrep mac80211_hwsim | wc -l) -eq 0 ]; then
