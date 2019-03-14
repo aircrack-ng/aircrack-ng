60832615c32c4a5f8ee16a3764479f323f3cec09
Joseph Benden <joe@benden.us>
2019-03-13 19:16:06 -0700

    autotools: Fix extra dist file inclusions.

--- test/test-aircrack-ng-0020.sh
+++ test/test-aircrack-ng-0020.sh
@@ -3,6 +3,9 @@
 # The next line is executed by /bin/sh, but not expect \
 exec ${EXPECT:=expect} "$0" ${1+"$@"}
 
+set abs_builddir $::env(abs_builddir)
+set abs_srcdir $::env(abs_srcdir)
+
 exit -onexit {
     exp_internal 1
     exec rm -f pipe.cap
@@ -13,9 +16,9 @@ flush stdout
 
 log_user 0
 
-exec cp test/wep_64_ptw_01.cap pipe.cap
+exec cp ${abs_srcdir}/wep_64_ptw_01.cap pipe.cap
 
-spawn ./aircrack-ng -X pipe.cap
+spawn ${abs_builddir}/../aircrack-ng -X pipe.cap
 set tool_spawn_id $spawn_id
 
 set timeout 15
@@ -26,11 +29,11 @@ expect "Opening pipe.cap"
 expect "got 2551 IVs"
 expect "Failed. Next try with 5000 IVs."
 
-exec cat test/wep_64_ptw_02.cap >> pipe.cap
+exec cat ${abs_srcdir}/wep_64_ptw_02.cap >> pipe.cap
 expect "Failed. Next try with 10000 IVs." { sleep 2 }
 
-exec cat test/wep_64_ptw_03.cap >> pipe.cap
-exec cat test/wep_64_ptw_04.cap >> pipe.cap
+exec cat ${abs_srcdir}/wep_64_ptw_03.cap >> pipe.cap
+exec cat ${abs_srcdir}/wep_64_ptw_04.cap >> pipe.cap
 expect "got 10180 IVs"
 expect {
     "Failed. Next try with 15000 IVs." { puts "OK\n"; exit 0 }
