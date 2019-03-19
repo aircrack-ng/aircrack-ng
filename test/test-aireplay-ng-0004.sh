ea978a2fd6365b016891a626ed0d2f9827e4da43
Mister X <3520734+Mister-X-@users.noreply.github.com>
2019-03-19 01:57:59 +0000

    Integration tests: Use common functions for aireplay-ng test 4

--- test/test-aireplay-ng-0004.sh
+++ test/test-aireplay-ng-0004.sh
@@ -3,64 +3,32 @@
 
 if test ! -z "${CI}"; then exit 77; fi
 
+ Load helper functions
+. "${abs_builddir}/../test/int-test-common.sh"
+
 # Check root
-if [ $(id -u) -ne 0 ]; then
-	echo "Not root, skipping"
-	exit 77
-fi
+check_root
 
-# Check hostapd is present
-hash hostapd 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-	echo "HostAPd is not installed, skipping"
-	exit 77
-fi
+# Check all required tools are installed
+check_airmon_ng_deps_present
+is_tool_present hostapd
 
-hash iw 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-	echo "iw is not installed, skipping"
-	exit 77
-fi
-
-hash lsusb 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-	echo "lsusb is not installed, skipping"
-	exit 77
-fi
-
-# Load module
-LOAD_MODULE=0
-if [ $(lsmod | egrep mac80211_hwsim | wc -l) -eq 0 ]; then
-	LOAD_MODULE=1
-	modprobe mac80211_hwsim radios=2 2>&1 >/dev/null
-	if [ $? -ne 0 ]; then
-		# XXX: It can fail if inside a container too
-		echo "Failed inserting module, skipping"
-		exit 77
-	fi
-fi
+# Load mac80211_hwsim
+load_module 2
 
 # Check there are two radios
-AMOUNT_RADIOS=$("${abs_builddir}/../scripts/airmon-ng" | egrep hwsim | wc -l)
-if [ ${AMOUNT_RADIOS} -ne 2 ]; then
-	echo "Expected two radios, got ${AMOUNT_RADIOS}, hwsim may be in use by something else, skipping"
-	exit 77
-fi
+check_radios_present 2
 
-# Check if interfaces are present and grab them
-WI_IFACE=$("${abs_builddir}/../scripts/airmon-ng" 2>/dev/null | egrep hwsim | head -n 1 | awk '{print $2}')
-WI_IFACE2=$("${abs_builddir}/../scripts/airmon-ng" 2>/dev/null | egrep hwsim | tail -n 1 | awk '{print $2}')
-if [ -z "${WI_IFACE}" ] || [ -z "${WI_IFACE2}" ]; then
-	echo "Failed getting interface names"
-	[ ${LOAD_MODULE} -eq 1 ] && rmmod mac80211_hwsim 2>&1 >/dev/null
-	exit 1
-fi
+# Get interfaces names
+get_hwsim_interface_name 1
+WI_IFACE=${IFACE}
+get_hwsim_interface_name 2
+WI_IFACE2=${IFACE}
 
 # Set-up hostapd
 SSID=thisrocks
 CHANNEL=1
-TEMP_HOSTAPD_CONF=$(mktemp)
-cat <<EOF > ${TEMP_HOSTAPD_CONF}
+cat <<EOF > ${TEMP_HOSTAPD_CONF_FILE}
 driver=nl80211
 interface=${WI_IFACE}
 channel=${CHANNEL}
@@ -69,22 +37,13 @@ ssid=${SSID}
 # Test 4
 EOF
 
-# Start it
-TEMP_HOSTAPD_PID="/tmp/hostapd_pid_$(date +%s)"
-hostapd -B ${TEMP_HOSTAPD_CONF} -P ${TEMP_HOSTAPD_PID} 2>&1
-if test $? -ne 0; then
-	echo "Failed starting hostapd"
-	echo "Running airmon-ng check kill may fix the issue"
-	[ ${LOAD_MODULE} -eq 1 ] && rmmod mac80211_hwsim 2>&1 >/dev/null
-	exit 1
-fi
+# Start HostAPd
+run_hostapd ${TEMP_HOSTAPD_CONF_FILE}
 
 
 # Put other interface in monitor mode
-ip link set ${WI_IFACE2} down
-iw dev ${WI_IFACE2} set monitor none
-ip link set ${WI_IFACE2} up
-iw dev ${WI_IFACE2} set channel ${CHANNEL}
+set_monitor_mode ${WI_IFACE2}
+set_interface_channel ${WI_IFACE2} ${CHANNEL}
 
 # Run actual test
 OUTPUT_TEMP=$(mktemp)
@@ -94,8 +53,7 @@ OUTPUT_TEMP=$(mktemp)
 	2>&1 > ${OUTPUT_TEMP}
 
 # Some cleanup
-kill -9 $(cat ${TEMP_HOSTAPD_PID} ) 2>&1 >/dev/null
-[ ${LOAD_MODULE} -eq 1 ] && rmmod mac80211_hwsim 2>&1 >/dev/null
+cleanup
 
 if [ -z "$(grep 'Injection is working!' ${OUTPUT_TEMP})" ]; then
 	echo "Injection is not working"
