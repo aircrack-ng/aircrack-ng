c4c1605c89b1354d5093c32246d24ca2e5977498
Mister X <3520734+Mister-X-@users.noreply.github.com>
2019-09-07 22:04:51 +0000

    Tests: Added WDS test for airdecap-ng

---
+++ test/test-airdecap-ng-0005.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+TMP_DEC=$(mktemp -u)
+TMP_MD5=$(mktemp -u)
+
+"${abs_builddir}/../airdecap-ng${EXEEXT}" \
+	"${abs_srcdir}/capture_wds-01.cap" \
+	-e test1 \
+	-p 12345678 \
+	-b 00:11:22:00:00:00 \
+	-o ${TMP_DEC} | \
+		cut -b 40- | \
+		 tr -d ' ' | \
+		md5sum | \
+		cut -b 1-32 > ${TMP_MD5}
+
+if [ "$(cat ${TMP_MD5})" != '45a93bc091a3929a7d63f86ddbb81401' ]; then
+	rm ${TMP_MD5} ${TMP_DEC}
+	echo "Unexpected airdecap-ng output"
+	exit 1
+fi
+
+rm ${TMP_MD5}
+if [ "$(md5sum ${TMP_DEC} | cut -b 1-32)" != '340b5bc23bec76e88f6a2df0cd2eeb33' ]; then
+	echo "Unexpected decrypted file"
+	rm ${TMP_DEC}
+	exit 1
+fi
+
+rm ${TMP_DEC}
+
+
+exit 0
+
