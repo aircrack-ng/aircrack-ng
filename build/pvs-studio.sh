f954388ace53e65a7812e1a9a6ab454a351a5b4e
Joseph Benden <joe@benden.us>
2019-09-13 12:50:55 -0700

    build: add exclude to PVS
    PVS should not report on copy-left license - we know. :)
    
    Signed-off-by: Joseph Benden <joe@benden.us>

--- build/pvs-studio.sh
+++ build/pvs-studio.sh
@@ -3,4 +3,4 @@
 set -euf
 
 pvs-studio-analyzer analyze -j8 -o PVS-Studio.log
-plog-converter -a 'GA:1,2,3;64:1,2,3;OP:1,2,3' -t errorfile PVS-Studio.log
+plog-converter -a 'GA:1,2,3;64:1,2,3;OP:1,2,3' -d V1042 -t errorfile PVS-Studio.log
