c1d3f6044f978dda7b78328ad8f468713725dbc3
Joseph Benden <joe@benden.us>
2020-10-09 12:28:56 -0700

    fix(osdep/cygwin_tap): fix mismatched signed comparisons
    Signed-off-by: Joseph Benden <joe@benden.us>

--- lib/osdep/cygwin_tap.c
+++ lib/osdep/cygwin_tap.c
@@ -132,9 +132,9 @@ static int ti_try_open(struct tip_cygwin * priv, char * guid)
 	if (!any && strcmp(priv->tc_guid, guid) != 0) return 0;
 
 	/* open the device */
-	const size_t device_len = snprintf(
+	const ssize_t device_len = snprintf(
 		device, sizeof(device), "%s%s%s", USERMODEDEVICEDIR, guid, TAPSUFFIX);
-	if (device_len == -1 || device_len >= sizeof(device)) return -1;
+	if (device_len == -1 || (size_t) device_len >= sizeof(device)) return -1;
 	h = CreateFile(device,
 				   GENERIC_READ | GENERIC_WRITE,
 				   0,
@@ -157,13 +157,13 @@ static int ti_try_open(struct tip_cygwin * priv, char * guid)
 	if (ti_media_status(priv, 1) == -1) goto out_err;
 
 	/* grab printable name */
-	size_t err = snprintf(priv->tc_name, sizeof(priv->tc_name) - 1, "%s", guid);
-	if (err == -1 || err >= sizeof(priv->tc_name)) goto out_err;
+	ssize_t err = snprintf(priv->tc_name, sizeof(priv->tc_name) - 1, "%s", guid);
+	if (err == -1 || (size_t) err >= sizeof(priv->tc_name)) goto out_err;
 
 	if (any)
 	{
 		err = snprintf(priv->tc_guid, sizeof(priv->tc_guid), "%s", guid);
-		if (err == -1 || err >= sizeof(priv->tc_guid)) goto out_err;
+		if (err == -1 || (size_t) err >= sizeof(priv->tc_guid)) goto out_err;
 	}
 
 	return 1;
@@ -200,9 +200,9 @@ static int ti_get_devs_component(struct tip_cygwin * priv, char * name)
 	char key[256];
 	int rc = 0;
 
-	const size_t key_len
+	const ssize_t key_len
 		= snprintf(key, sizeof(key) - 1, "%s\\%s", ADAPTER_KEY, name);
-	if (key_len == -1 || key_len >= sizeof(key)) return -1;
+	if (key_len == -1 || (size_t) key_len >= sizeof(key)) return -1;
 	if (RegOpenKeyEx(
 			HKEY_LOCAL_MACHINE, key, 0, KEY_READ | KEY_WRITE, &priv->tc_key)
 		!= ERROR_SUCCESS)
@@ -400,8 +400,8 @@ static int ti_set_mtu_cygwin(struct tif * ti, int mtu)
 	char * key = "MTU";
 
 	/* check if reg remains unchanged to avoid reset */
-	const size_t m_len = snprintf(m, sizeof(m) - 1, "%d", mtu);
-	if (m_len == -1 || m_len >= sizeof(m)) return -1;
+	const ssize_t m_len = snprintf(m, sizeof(m) - 1, "%d", mtu);
+	if (m_len == -1 || (size_t) m_len >= sizeof(m)) return -1;
 	if (ti_read_reg(priv, key, mold, sizeof(mold)) != -1)
 	{
 		if (strcmp(m, mold) == 0) return 0;
@@ -645,9 +645,9 @@ static struct tif * ti_open_cygwin(char * iface)
 	/* setup iface */
 	if (iface)
 	{
-		const size_t err
+		const ssize_t err
 			= snprintf(priv->tc_guid, sizeof(priv->tc_guid), "%s", iface);
-		if (err == -1 || err >= sizeof(priv->tc_guid)) goto err;
+		if (err == -1 || (size_t) err >= sizeof(priv->tc_guid)) goto err;
 	}
 	if (ti_do_open_cygwin(priv) == -1) goto err;
 
