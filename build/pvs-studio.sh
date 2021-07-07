73ef945b5ab56ee4029787d363d773cf15b8e560
Joseph Benden <joe@benden.us>
2021-07-07 09:13:00 -0700

    build: script should mirror CI use
    Signed-off-by: Joseph Benden <joe@benden.us>

--- build/pvs-studio.sh
+++ build/pvs-studio.sh
@@ -2,8 +2,8 @@
 
 set -euf
 
-pvs-studio-analyzer analyze -a "GA;OP;CS" -j8 -o PVS-Studio.log
+pvs-studio-analyzer analyze -a "GA;OP;CS" -s .pvs-suppress -j8 -o PVS-Studio.log
 plog-converter -a 'GA:1,2,3;64:1,2,3;OP:1,2,3' -d V1032,V1042,V597 -t errorfile PVS-Studio.log
 plog-converter -a 'GA:1,2,3;64:1,2,3;OP:1,2,3' -d V1032,V1042,V597 -t csv -o pvs-report.csv PVS-Studio.log
 
-./build/count-pvs-bugs.py pvs-report.csv 94
+./build/count-pvs-bugs.py pvs-report.csv 98
