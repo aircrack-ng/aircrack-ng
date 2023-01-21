43166b96e7562bb7186840dfc653355359d40839
Andras Gemes <andrasgemes@outlook.com>
2023-01-22 00:33:45 +0100

    airdrop-ng/setup.py: switch from distutils to setuptools

--- scripts/airdrop-ng/setup.py
+++ scripts/airdrop-ng/setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # This file is Copyright David Francos Cuartero, licensed under the GPL2 license.
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='airdrop-ng',
       version='1.1',
       description='Rule based Deauth Tool',
       author='TheX1le',
-      console = [{"script": "airdrop-ng" }],
       url='https://aircrack-ng.org',
       license='GPL2',
       classifiers=[ 'Development Status :: 4 - Beta', ],
