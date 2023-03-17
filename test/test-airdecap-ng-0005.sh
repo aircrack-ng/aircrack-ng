7e6160e9d386077482b47b6411f4c0a07cc1eee5
Andras Gemes <andrasgemes@outlook.com>
2023-03-17 11:18:34 +0100

    airdecap-ng: remove debug code from test-0005.sh

--- test/test-airdecap-ng-0005.sh
+++ test/test-airdecap-ng-0005.sh
@@ -18,16 +18,6 @@ airdecap_output=$("${abs_builddir}/../airdecap-ng${EXEEXT}" \
 # shellcheck disable=SC2181
 if [ $? != 0 ]; then
   echo "$airdecap_output"
-  CAP_MD5=$(md5sum "${abs_srcdir}"/capture_wds-01.cap | cut -b 1-32)
-  # shellcheck disable=SC2012
-  CAP_SIZE=$(ls -l "${abs_srcdir}"/capture_wds-01.cap | cut -d " " -f5)
-  if [ "${CAP_MD5}" != '9f5d20d70a5d27b8de1b094cec77b8dd' ]; then
-    echo "Corrupt .cap file"
-    echo "Expected .cap MD5 hash: 9f5d20d70a5d27b8de1b094cec77b8dd"
-    echo "Actual .cap MD5 hash: ${CAP_MD5}"
-    echo "Expected .cap size: 21113 bytes"
-    echo "Actual .cap size: ${CAP_SIZE} bytes"
-  fi
   exit 1
 else
   echo "$airdecap_output" | \
@@ -41,8 +31,6 @@ if [ "$(cat ${TMP_MD5})" != '45a93bc091a3929a7d63f86ddbb81401' ]; then
 	#rm ${TMP_MD5} ${TMP_DEC}
 	echo "Unexpected airdecap-ng output:"
 	echo "$airdecap_output"
-	echo "Expected MD5 hash: 45a93bc091a3929a7d63f86ddbb81401"
-	echo "Actual MD5 hash: ${TMP_MD5}"
 	echo "Decrypted file: ${TMP_DEC}"
 	exit 1
 fi
