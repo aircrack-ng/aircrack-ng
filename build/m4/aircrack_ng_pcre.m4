aircrack-ng/aircrack-ng

commit b381ef3f6b6cc83a4aa016f4c0aebb58fcffcf3f

autotools: indicate if PCRE or PCRE2 is being used

build/m4/aircrack_ng_pcre.m4
@@ -55,7 +55,29 @@ else
 	PKG_CHECK_MODULES(PCRE, libpcre, HAVE_PCRE=yes, HAVE_PCRE=no)
 fi
 
-AS_IF([test "x$HAVE_PCRE" = "xyes"], [
+AC_ARG_ENABLE(static-pcre2,
+    AS_HELP_STRING([--enable-static-pcre2],
+		[Enable statically linked PCRE2 libpcre2-8.]),
+    [static_pcre2=$enableval], [static_pcre2=no])
+
+if test "x$static_pcre2" != "xno"; then
+	AC_REQUIRE([AX_EXT_HAVE_STATIC_LIB_DETECT])
+	AX_EXT_HAVE_STATIC_LIB(PCRE2, ${DEFAULT_STATIC_LIB_SEARCH_PATHS}, pcre2 libpcre2-8, pcre2_version)
+	if test "x$PCRE2_FOUND" = xyes; then
+		HAVE_PCRE2=yes
+	else
+		HAVE_PCRE2=no
+	fi
+else
+	PKG_CHECK_MODULES(PCRE2, libpcre2-8, HAVE_PCRE2=yes, HAVE_PCRE2=no)
+fi
+
+if test "x$HAVE_PCRE" = "xyes" && test "x$HAVE_PCRE2" = "xyes"; then
+    AC_DEFINE([HAVE_PCRE2], [1], [Define this if you have libpcre2-8 on your system])
+    PCRE2_NOTE="(Pcre and Pcre2 found, using Pcre2)"
+elif test "x$HAVE_PCRE" = "xyes"; then
     AC_DEFINE([HAVE_PCRE], [1], [Define this if you have libpcre on your system])
-])
+elif test "x$HAVE_PCRE2" = "xyes"; then
+    AC_DEFINE([HAVE_PCRE2], [1], [Define this if you have libpcre2-8 on your system])
+fi
 ])

build/m4/aircrack_ng_pcre2.m4
@@ -1,61 +1,0 @@
-dnl Aircrack-ng
-dnl
-dnl Copyright (C) 2023 Andras Gemes <andrasgemes@outlook.com>
-dnl
-dnl Autotool support was written by: Joseph Benden <joe@benden.us>
-dnl
-dnl This program is free software; you can redistribute it and/or modify
-dnl it under the terms of the GNU General Public License as published by
-dnl the Free Software Foundation; either version 2 of the License, or
-dnl (at your option) any later version.
-dnl
-dnl This program is distributed in the hope that it will be useful,
-dnl but WITHOUT ANY WARRANTY; without even the implied warranty of
-dnl MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-dnl GNU General Public License for more details.
-dnl
-dnl You should have received a copy of the GNU General Public License
-dnl along with this program; if not, write to the Free Software
-dnl Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
-dnl
-dnl In addition, as a special exception, the copyright holders give
-dnl permission to link the code of portions of this program with the
-dnl OpenSSL library under certain conditions as described in each
-dnl individual source file, and distribute linked combinations
-dnl including the two.
-dnl
-dnl You must obey the GNU General Public License in all respects
-dnl for all of the code used other than OpenSSL.
-dnl
-dnl If you modify file(s) with this exception, you may extend this
-dnl exception to your dnl version of the file(s), but you are not obligated
-dnl to do so.
-dnl
-dnl If you dnl do not wish to do so, delete this exception statement from your
-dnl version.
-dnl
-dnl If you delete this exception statement from all source files in the
-dnl program, then also delete it here.
-
-AC_DEFUN([AIRCRACK_NG_PCRE2], [
-AC_ARG_ENABLE(static-pcre2,
-    AS_HELP_STRING([--enable-static-pcre2],
-		[Enable statically linked PCRE2 libpcre2-8.]),
-    [static_pcre2=$enableval], [static_pcre2=no])
-
-if test "x$static_pcre2" != "xno"; then
-	AC_REQUIRE([AX_EXT_HAVE_STATIC_LIB_DETECT])
-	AX_EXT_HAVE_STATIC_LIB(PCRE2, ${DEFAULT_STATIC_LIB_SEARCH_PATHS}, pcre2 libpcre2-8, pcre2_version)
-	if test "x$PCRE2_FOUND" = xyes; then
-		HAVE_PCRE2=yes
-	else
-		HAVE_PCRE2=no
-	fi
-else
-	PKG_CHECK_MODULES(PCRE2, libpcre2-8, HAVE_PCRE2=yes, HAVE_PCRE2=no)
-fi
-
-AS_IF([test "x$HAVE_PCRE2" = "xyes"], [
-    AC_DEFINE([HAVE_PCRE2], [1], [Define this if you have libpcre2-8 on your system])
-])
-])

configure.ac
@@ -143,7 +143,6 @@ AIRCRACK_NG_EXT_SCRIPTS
 AIRCRACK_NG_HWLOC
 AIRCRACK_NG_PCAP
 AIRCRACK_NG_PCRE
-AIRCRACK_NG_PCRE2
 AIRCRACK_NG_RFKILL
 AIRCRACK_NG_SQLITE
 AIRCRACK_NG_ZLIB
@@ -320,7 +319,7 @@ ${PACKAGE} ${VERSION}
     Jemalloc:                    ${JEMALLOC}
     Pcap:                        ${PCAP_FOUND}
     Pcre:                        ${HAVE_PCRE}
-    Pcre2:                       ${HAVE_PCRE2}
+    Pcre2:                       ${HAVE_PCRE2} ${PCRE2_NOTE}
     Sqlite:                      ${HAVE_SQLITE3}
     Tcmalloc:                    ${TCMALLOC}
     Zlib:                        ${HAVE_ZLIB}

commit b8d0b8cb6caa6940443b3e6ca32efc78d0c9d00e

autotools: reset PCRE CFLAGS/LIBS with both PCRE and PCRE2 present
Commit b381ef3f6b6c ("autotools: indicate if PCRE or PCRE2 is being
used") fixed a case where both pcre and pcre2 library are detected and
put a preference on using pcre2.

Although the commit fix this corner case, there is still a latent
problem with trying to link/include both library. This is caused by the
fact that in the Makefile.inc for src and lib, we include both
PCRE_CFLAGS and PCRE2_CFLAGS and PCRE_LIBS and PCRE2_LIBS for each
tool/lib.

To handle this and not bloat the Makefile with additional condition,
simply reset the PCRE_CFLAGS and PCRE_LIBS in case where we detect both
library and we prefer to use pcre2.

Fixes: b381ef3f6b6c ("autotools: indicate if PCRE or PCRE2 is being used")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

build/m4/aircrack_ng_pcre.m4
@@ -75,6 +75,10 @@ fi
 if test "x$HAVE_PCRE" = "xyes" && test "x$HAVE_PCRE2" = "xyes"; then
     AC_DEFINE([HAVE_PCRE2], [1], [Define this if you have libpcre2-8 on your system])
     PCRE2_NOTE="(Pcre and Pcre2 found, using Pcre2)"
+    # Reset PCRE cflags and libs variables as we include both PCRE and PCRE2 in Makefile.inc
+    # and would result in trying to link/include both library.
+    PCRE_CFLAGS=""
+    PCRE_LIBS=""
 elif test "x$HAVE_PCRE" = "xyes"; then
     AC_DEFINE([HAVE_PCRE], [1], [Define this if you have libpcre on your system])
 elif test "x$HAVE_PCRE2" = "xyes"; then