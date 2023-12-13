b43d35cbcb93565b4817e57eb8facc8ba051e140
Mister-X- <3520734+Mister-X-@users.noreply.github.com>
2023-12-13 13:24:28 -0600

    Docker Gentoo: ignore some shellcheck warnings.

--- docker_package_install.sh
+++ docker_package_install.sh
@@ -137,6 +137,7 @@ elif [ "${ID}" = 'gentoo' ]; then
 priority = 9999
 sync-uri = https://gentoo.osuosl.org/experimental/amd64/binpkg/default/linux/17.1/x86-64/
 EOF
+        # shellcheck disable=SC2016
         echo 'FEATURES="${FEATURES} -pid-sandbox"' >> /etc/portage/make.conf
         emerge --sync >/dev/null
         emerge app-portage/elt-patches dev-db/sqlite dev-lang/python dev-libs/libbsd dev-libs/libnl dev-libs/libpcre2 \
@@ -150,6 +151,7 @@ EOF
 priority = 9999
 sync-uri = https://gentoo.osuosl.org/experimental/amd64/binpkg/default/linux/17.1/x86-64/
 EOF
+        # shellcheck disable=SC2016
         echo 'FEATURES="${FEATURES} -pid-sandbox"' >> /etc/portage/make.conf
         emerge --sync >/dev/null
         emerge dev-db/sqlite dev-lang/python dev-libs/libbsd dev-libs/libnl dev-libs/libpcre2 dev-libs/openssl \
