f579fb3fcb21c5eeb7aa740b717851f46f373eac
Mister-X- <3520734+Mister-X-@users.noreply.github.com>
2023-12-16 12:36:44 -0600

    ignore sysconf(_SC_ARG_MAX) when value is -1
    On Cygwin, it returns -1

--- src/aircrack-ng/session.c
+++ src/aircrack-ng/session.c
@@ -202,7 +202,7 @@ int ac_session_set_amount_arguments(struct session * session, const char * str)
 	// Parse amount of arguments
 	int nb_input_scanned = sscanf(str, "%d", &(session->argc));
 	if (nb_input_scanned != 1 || session->argc < SESSION_MIN_NBARG
-		|| session->argc > sysconf(_SC_ARG_MAX))
+		|| (sysconf(_SC_ARG_MAX) != -1 && session->argc > sysconf(_SC_ARG_MAX)))
 	{
 		// There should be at least 4 arguments:
 		// - Executable path (argv[0])
